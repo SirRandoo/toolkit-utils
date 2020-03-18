dd29b757505494313c86dc80d43817ca47e93d9b
SirRandoo <randomshovel@gmail.com>
2020-03-18 13:50:46 -0500

    Added ability to purge viewer data.

---
+++ Source/Utils/Constraints/BannedConstraint.cs
@@ -0,0 +1,21 @@
+﻿using TwitchToolkit;
+
+using UnityEngine;
+
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Constraints
+{
+    public class BannedConstraint : ConstraintBase
+    {
+        public override void Draw(Rect canvas)
+        {
+            Widgets.Label(canvas, "TKUtils.Windows.Purge.Constraints.Banned".Translate());
+        }
+
+        public override bool ShouldPurge(Viewer viewer)
+        {
+            return viewer.IsBanned;
+        }
+    }
+}
---
+++ Source/Windows/Dialog_PurgeViewers.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using SirRandoo.ToolkitUtils.Constraints;
+
+using TwitchToolkit;
+
+using UnityEngine;
+
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Windows
+{
+    public class Dialog_PurgeViewers : Window
+    {
+        private static readonly List<Tuple<string, Type>> registry = new List<Tuple<string, Type>>
+        {
+            new Tuple<string, Type>("Banned", typeof(BannedConstraint)),
+            new Tuple<string, Type>("Coin", typeof(CoinConstraint)),
+            new Tuple<string, Type>("Karma", typeof(KarmaConstraint)),
+            new Tuple<string, Type>("Mod", typeof(ModConstraint)),
+            new Tuple<string, Type>( "Name", typeof(NameConstraint)),
+            new Tuple<string, Type>("Subscriber", typeof(SubscriberConstraint)),
+            new Tuple<string, Type>("Time", typeof(TimeConstraint)),
+            new Tuple<string, Type>("Vip", typeof(VipConstraint))
+        };
+
+        private List<ConstraintBase> constraints;
+        private Vector2 scrollPos = Vector2.zero;
+        private bool showingAffected = false;
+
+        public Dialog_PurgeViewers()
+        {
+            doCloseX = true;
+            forcePause = true;
+
+            constraints = new List<ConstraintBase>();
+        }
+
+        public override Vector2 InitialSize => new Vector2(900f, 740f);
+
+        public override void DoWindowContents(Rect inRect)
+        {
+            var midpoint = inRect.width / 2;
+            var buttonText = (showingAffected ? "TKUtils.Windows.Purge.Buttons.Confirm" : "TKUtils.Windows.Purge.Buttons.Execute").Translate();
+            var buttonWidth = Text.CalcSize(buttonText).x * 1.5f;
+            var buttonRect = new Rect(midpoint - (buttonWidth / 2f), inRect.height - 30f, buttonWidth, 28f);
+            var headerArea = new Rect(inRect.x, inRect.y, inRect.width, 28f);
+            var contentArea = new Rect(inRect.x, headerArea.height + 10f, inRect.width, inRect.height - headerArea.height - 40f);
+            var contentView = new Rect(contentArea.x, contentArea.y, contentArea.width * 0.97f, 96f * 36f);
+
+            if(Widgets.ButtonText(buttonRect, buttonText))
+            {
+                if(showingAffected)
+                {
+                    Purge();
+                }
+                else
+                {
+                    showingAffected = true;
+                }
+            }
+
+            DrawHeader(headerArea);
+
+            Widgets.BeginScrollView(contentArea, ref scrollPos, contentView);
+            if(showingAffected)
+            {
+                var affected = GetAffectedViewers();
+                var exemptText = "TKUtils.Windows.Purge.Buttons.ExemptViewer".Translate();
+                var exemptWidth = Text.CalcSize(exemptText).x * 1.5f;
+
+                for(var i = 0; i < affected.Length; i++)
+                {
+                    var viewer = affected[i];
+
+                    Widgets.Label(new Rect(contentView.x, contentView.y + (i * 40f), inRect.width - exemptWidth - 20f, 30f), viewer.username);
+
+                    if(Widgets.ButtonText(new Rect(contentView.width - exemptWidth, contentView.y + (i * 40f), exemptWidth, 30f), exemptText))
+                    {
+                        var c = new NameConstraint();
+                        c.SetUsername(viewer.username);
+                        c.SetComparison(NameComparisonTypes.Not);
+
+                        constraints.Add(c);
+                    }
+                }
+            }
+            else
+            {
+                var exemptText = "TKUtils.Windows.Purge.Buttons.Remove".Translate();
+                var exemptWidth = Text.CalcSize(exemptText).x * 1.5f;
+                ConstraintBase toRemove = null;
+
+                for(var i = 0; i < constraints.Count; i++)
+                {
+                    var constraint = constraints[i];
+
+                    constraint.Draw(new Rect(contentView.x, contentView.y + (i * 40f), contentView.width - exemptWidth - 20f, 30f));
+
+                    if(Widgets.ButtonText(new Rect(contentView.width - exemptWidth, contentView.y + (i * 40f), exemptWidth, 30f), exemptText))
+                    {
+                        toRemove = constraint;
+                    }
+                }
+
+                if(toRemove != null)
+                {
+                    constraints.Remove(toRemove);
+                    toRemove = null;
+                }
+            }
+            Widgets.EndScrollView();
+        }
+
+        public void DrawHeader(Rect region)
+        {
+            if(showingAffected)
+            {
+                var backText = "TKUtils.Windows.Purge.Buttons.Back".Translate();
+                var backRegion = new Rect(region.x, region.y, Text.CalcSize(backText).x * 1.5f, region.height);
+                var textRegion = new Rect(region.x + backRegion.width + 10f, region.y, region.width - backRegion.width - 10f, region.height);
+
+                if(Widgets.ButtonText(backRegion, backText))
+                {
+                    showingAffected = false;
+                }
+
+                var affected = GetAffectedViewers();
+                Widgets.Label(textRegion, "TKUtils.Windows.Purge.Headers.Affected".Translate(affected.Length.Named("COUNT")));
+            }
+            else
+            {
+                var width = region.width * 0.25f;
+
+                if(Widgets.ButtonText(new Rect(region.x, region.y, width, region.height), "TKUtils.Windows.Purge.Buttons.Add".Translate()))
+                {
+                    var keys = registry.Select(i => i.Item1).ToArray();
+                    var options = new List<FloatMenuOption>();
+
+                    foreach(var key in keys)
+                    {
+                        options.Add(
+                            new FloatMenuOption(
+                                key,
+                                delegate
+                                {
+                                    var constraint = registry.Where(i => i.Item1.Equals(key)).First();
+                                    var t = constraint.Item2;
+
+                                    constraints.Add((ConstraintBase) Activator.CreateInstance(t));
+                                }
+                            )
+                        );
+                    }
+
+                    Find.WindowStack.Add(new FloatMenu(options));
+                }
+
+                if(Widgets.ButtonText(new Rect(region.x + width + 10f, region.y, width, region.height), "TKUtils.Windows.Purge.Buttons.ClearConstraints".Translate()))
+                {
+                    constraints.Clear();
+                }
+            }
+        }
+
+        public Viewer[] GetAffectedViewers()
+        {
+            return Viewers.All
+                .Where(v => constraints.All(c => c.ShouldPurge(v)))
+                .ToArray();
+        }
+
+        private void Purge()
+        {
+            var affected = GetAffectedViewers();
+            var count = 0;
+
+            foreach(var viewer in affected)
+            {
+                if(Viewers.All.Remove(viewer))
+                {
+                    count += 1;
+                }
+            }
+
+            Logger.Warn($"Purged {count} viewers out of the requested {affected.Length}!");
+            showingAffected = false;
+        }
+    }
+}
