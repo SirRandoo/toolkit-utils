b9d0051fe051d05210aefb8677c715892b25b78d
SirRandoo <randomshovel@gmail.com>
2020-03-24 17:52:47 -0500

    Redesigned Utils' setting dialog.

--- Source/TKSettings.cs
+++ Source/TKSettings.cs
@@ -1,8 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Text;
+using UnityEngine;
 using Verse;
 
 namespace SirRandoo.ToolkitUtils
 {
+    public enum Categories { General, CommandTweaks, PawnCommands }
+
     public class TkSettings : ModSettings
     {
         public static bool Commands = true;
@@ -17,54 +20,210 @@ namespace SirRandoo.ToolkitUtils
         public static bool ShowSurgeries = true;
         public static bool ShowWeapon = true;
         public static bool SortWorkPriorities;
+        public static bool Race = true;
         public static bool TempInGear;
         public static int LookupLimit = 10;
         public static bool VersionedModList;
-        private static Vector2 _scrollPos = Vector2.zero;
+
+        private static Categories _category = Categories.General;
 
         public static void DoWindowContents(Rect inRect)
         {
+            var catRect = new Rect(inRect.x, inRect.y, inRect.width * 0.25f, inRect.height);
+            var setRect = new Rect(
+                catRect.width + 10f,
+                inRect.y,
+                inRect.width - catRect.width - 10f,
+                inRect.height
+            );
+
             var listing = new Listing_Standard();
-            var view = new Rect(inRect.x, inRect.y, inRect.width * 0.9f, 26f * 36f);
+            Widgets.DrawMenuSection(catRect);
 
-            listing.BeginScrollView(inRect, ref _scrollPos, ref view);
+            listing.Begin(catRect);
+            var generalRect = listing.Label("TKUtils.SettingGroups.General".Translate());
+            var commandTweakRect = listing.Label("TKUtils.SettingGroups.CommandTweaks".Translate());
+            var pawnRect = listing.Label("TKUtils.SettingGroups.PawnCommands".Translate());
 
-            listing.Label("TKUtils.Groups.CommandTweaks.Label".Translate());
-            listing.GapLine();
+            if (Widgets.ButtonInvisible(generalRect) || _category == Categories.General)
+            {
+                _category = Categories.General;
 
-            listing.CheckboxLabeled("TKUtils.CommandTweaks.Emojis.Label".Translate(), ref Emojis);
-            listing.CheckboxLabeled("TKUtils.CommandTweaks.DecorateUtils.Label".Translate(), ref DecorateUtils);
-            listing.CheckboxLabeled("TKUtils.CommandTweaks.VersionedModList.Label".Translate(), ref VersionedModList);
-            listing.CheckboxLabeled("TKUtils.CommandTweaks.RichText.Label".Translate(), ref RichText);
-            listing.CheckboxLabeled("TKUtils.CommandTweaks.Label".Translate(), ref Commands);
+                Widgets.DrawHighlight(generalRect);
+            }
+
+            if (Widgets.ButtonInvisible(commandTweakRect) || _category == Categories.CommandTweaks)
+            {
+                _category = Categories.CommandTweaks;
+
+                Widgets.DrawHighlight(commandTweakRect);
+            }
+
+            if (Widgets.ButtonInvisible(pawnRect) || _category == Categories.PawnCommands)
+            {
+                _category = Categories.PawnCommands;
+
+                Widgets.DrawHighlight(pawnRect);
+            }
+
+            listing.End();
+            
+            switch (_category)
+            {
+                case Categories.General:
+                    DrawGeneral(setRect);
+                    break;
+                case Categories.CommandTweaks:
+                    DrawCommandTweaks(setRect);
+                    break;
+                case Categories.PawnCommands:
+                    DrawPawnCommands(setRect);
+                    break;
+            }
+        }
+
+        private static void DrawGeneral(Rect canvas)
+        {
+            var listing = new Listing_Standard();
+
+            listing.Begin(canvas);
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.General.VersionedModList.Label".Translate(),
+                ref VersionedModList,
+                "TKUtils.SettingGroups.General.VersionedModList.Tooltip".Translate()
+            );
+
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.General.Emojis.Label".Translate(),
+                ref Emojis,
+                "TKUtils.SettingGroups.General.Emojis.Tooltip".Translate()
+            );
+
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.General.DecorateUtils.Label".Translate(),
+                ref DecorateUtils,
+                "TKUtils.SettingGroups.General.DecorateUtils.Tooltip".Translate()
+            );
+
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.General.Sexuality.Label".Translate(),
+                ref Sexuality,
+                "TKUtils.SettingGroups.General.Sexuality.Tooltip".Translate()
+            );
+
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.General.Race.Label".Translate(),
+                ref Race,
+                "TKUtils.SettingGroups.General.Race.Tooltip".Translate()
+            );
+
+            var buffer = LookupLimit.ToString();
+            var line = listing.GetRect(Text.LineHeight);
+            var labelRect = new Rect(line.x, line.y, line.width * 0.85f, line.height);
+            var entryRect = new Rect(
+                line.x + labelRect.width + 5f,
+                line.y,
+                line.width - labelRect.width - 5f,
+                line.height
+            );
+            
+            Widgets.Label(labelRect, "TKUtils.SettingGroups.General.LookupLimit".Translate());
+            Widgets.TextFieldNumeric(entryRect, ref LookupLimit, ref buffer);
+            
+            listing.End();
+        }
+
+        private static void DrawCommandTweaks(Rect canvas)
+        {
+            var listing = new Listing_Standard();
+            listing.Begin(canvas);
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.CommandTweaks.Unrichify.Label".Translate(),
+                ref RichText,
+                "TKUtils.SettingGroups.CommandTweaks.Unrichify.Tooltip".Translate()
+            );
+
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.CommandTweaks.Parser.Label".Translate(),
+                ref Commands,
+                "TKUtils.SettingGroups.CommandTweaks.Parser.Tooltip".Translate()
+            );
 
             if (Commands)
             {
-                Prefix = listing.TextEntryLabeled("TKUtils.CommandTweaks.CommandPrefix.Label".Translate(), Prefix);
+                var line = listing.GetRect(Text.LineHeight);
+                var labelRect = new Rect(line.x, line.y, line.width * 0.85f, line.height);
+                var entryRect = new Rect(
+                    line.x + labelRect.width + 5f,
+                    line.y,
+                    line.width - labelRect.width - 5f,
+                    line.height
+                );
 
-                if (Prefix.StartsWith(" ") || Prefix.StartsWith("/") || Prefix.StartsWith("."))
+                Widgets.Label(labelRect, "TKUtils.SettingGroups.CommandTweaks.Prefix".Translate());
+                Prefix = Widgets.TextField(entryRect, Prefix);
+
+                if (Prefix.StartsWith(" "))
+                {
+                    Prefix = Prefix.Trim();
+                }
+
+                if (Prefix.StartsWith("/") || Prefix.StartsWith("."))
                 {
                     Prefix = "!";
-                    Logger.Warn("User attempted to use a reserved character in their command prefix!  Resetting...");
                 }
             }
 
-            listing.Gap(24);
+            listing.End();
+        }
 
-            listing.Label("TKUtils.Groups.PawnCommands.Label".Translate());
-            listing.GapLine();
+        private static void DrawPawnCommands(Rect canvas)
+        {
+            var listing = new Listing_Standard();
 
-            listing.CheckboxLabeled("TKUtils.PawnCommands.Traits.Sexuality.Label".Translate(), ref Sexuality);
-            listing.CheckboxLabeled("TKUtils.PawnCommands.Body.TempInGear.Label".Translate(), ref TempInGear);
-            listing.CheckboxLabeled("TKUtils.PawnCommands.Gear.ShowApparel.Label".Translate(), ref ShowApparel);
-            listing.CheckboxLabeled("TKUtils.PawnCommands.Gear.ShowArmor.Label".Translate(), ref ShowArmor);
-            listing.CheckboxLabeled("TKUtils.PawnCommands.Gear.ShowWeapon.Label".Translate(), ref ShowWeapon);
-            listing.CheckboxLabeled("TKUtils.PawnCommands.Health.ShowSurgeries.Label".Translate(), ref ShowSurgeries);
-            listing.CheckboxLabeled("TKUtils.PawnCommands.Work.Sort.Label".Translate(), ref SortWorkPriorities);
-            listing.CheckboxLabeled("TKUtils.PawnCommands.Work.Filter.Label".Translate(), ref FilterWorkPriorities);
-            listing.Gap(24);
+            listing.Begin(canvas);
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.PawnCommands.TempInGear.Label".Translate(),
+                ref TempInGear,
+                "TKUtils.SettingGroups.PawnCommands.TempInGear.Tooltip".Translate()
+            );
 
-            listing.EndScrollView(ref view);
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.PawnCommands.ShowApparel.Label".Translate(),
+                ref ShowApparel,
+                "TKUtils.SettingGroups.PawnCommands.ShowApparel.Tooltip".Translate()
+            );
+
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.PawnCommands.ShowArmor.Label".Translate(),
+                ref ShowArmor,
+                "TKUtils.SettingGroups.PawnCommands.ShowArmor.Tooltip".Translate()
+            );
+
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.PawnCommands.ShowWeapon.Label".Translate(),
+                ref ShowWeapon,
+                "TKUtils.SettingGroups.PawnCommands.ShowWeapon.Tooltip".Translate()
+            );
+
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.PawnCommands.ShowSurgeries.Label".Translate(),
+                ref ShowSurgeries,
+                "TKUtils.SettingGroups.PawnCommands.ShowSurgeries.Tooltip".Translate()
+            );
+
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.PawnCommands.SortWork.Label".Translate(),
+                ref SortWorkPriorities,
+                "TKUtils.SettingGroups.PawnCommands.SortWork.Tooltip".Translate()
+            );
+
+            listing.CheckboxLabeled(
+                "TKUtils.SettingGroups.PawnCommands.FilterWork.Label".Translate(),
+                ref FilterWorkPriorities,
+                "TKUtils.SettingGroups.PawnCommands.FilterWork.Tooltip".Translate()
+            );
+            listing.End();
         }
 
         public override void ExposeData()
@@ -83,8 +242,9 @@ namespace SirRandoo.ToolkitUtils
             Scribe_Values.Look(ref Sexuality, "sexuality", true);
             Scribe_Values.Look(ref SortWorkPriorities, "sortWork");
             Scribe_Values.Look(ref FilterWorkPriorities, "filterWork");
-            
+
             Scribe_Values.Look(ref LookupLimit, "lookupLimit", 10);
+            Scribe_Values.Look(ref Race, "race", true);
         }
     }
 }
