228a18e05dcc17eb23a7e5f513ac88d166ad2705
SirRandoo <randomshovel@gmail.com>
2020-03-24 15:20:29 -0500

    Overwrote mypawnstory and mypawnskills

---
+++ Source/Commands/PawnSkillsCommand.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using SirRandoo.ToolkitUtils.Utils;
+using TwitchToolkit;
+using TwitchToolkit.IRC;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Commands
+{
+    public class PawnSkillsCommand : CommandBase
+    {
+        public override void RunCommand(IRCMessage message)
+        {
+            if (!CommandsHandler.AllowCommand(message))
+            {
+                return;
+            }
+
+            var pawn = GetOrFindPawn(message.User);
+
+            if (pawn == null)
+            {
+                message.Reply("TKUtils.Responses.NoPawn".Translate());
+                return;
+            }
+
+            var parts = new List<string>();
+            var skills = pawn.skills.skills;
+
+            foreach (var skill in skills)
+            {
+                var container = "";
+
+                switch (skill.passion)
+                {
+                    case Passion.Major:
+                        container += "🔥🔥".AltText("++");
+                        break;
+                    case Passion.Minor:
+                        container += "🔥".AltText("+");
+                        break;
+                }
+
+                container += "TKUtils.Formats.KeyValue".Translate(
+                    skill.def.LabelCap,
+                    skill.TotallyDisabled
+                        ? "-"
+                        : skill.levelInt.ToString()
+                ).RawText;
+
+                parts.Add(container);
+            }
+            
+            message.Reply(string.Join(", ", parts.ToArray()).WithHeader("StatsReport_Skills".Translate()));
+        }
+    }
+}
---
+++ Source/Commands/PawnStoryCommand.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SirRandoo.ToolkitUtils.Utils;
+using TwitchToolkit;
+using TwitchToolkit.IRC;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Commands
+{
+    public class PawnStoryCommand : CommandBase
+    {
+        public override void RunCommand(IRCMessage message)
+        {
+            if (!CommandsHandler.AllowCommand(message))
+            {
+                return;
+            }
+
+            var pawn = GetOrFindPawn(message.User);
+
+            if (pawn == null)
+            {
+                message.Reply("TKUtils.Responses.NoPawn".Translate().WithHeader("TabCharacter".Translate()));
+                return;
+            }
+
+            var parts = new List<string>
+            {
+                string.Join(
+                    ", ".Translate(),
+                    pawn.story.AllBackstories.Select(b => b.title).ToArray()
+                )
+            };
+
+            switch (pawn.gender)
+            {
+                case Gender.Female:
+                    parts.Add("♀".AltText("Female".Translate()));
+                    break;
+                case Gender.Male:
+                    parts.Add("♂".AltText("Male".Translate()));
+                    break;
+                case Gender.None:
+                    parts.Add("⚪".AltText("NoneLower".Translate()));
+                    break;
+            }
+
+            var workTags = pawn.story.DisabledWorkTagsBackstoryAndTraits;
+
+            if (workTags == WorkTags.None)
+            {
+                parts.Add($"{"IncapableOf".Translate().RawText} {"NoneLower".Translate().RawText}");
+            }
+            else
+            {
+                var filteredTags = pawn.story.DisabledWorkTagsBackstoryAndTraits.GetAllSelectedItems<WorkTags>()
+                    .Where(t => t != WorkTags.None)
+                    .Select(t => t.LabelTranslated().CapitalizeFirst())
+                    .ToArray();
+
+                parts.Add($"{"IncapableOf".Translate().RawText} {string.Join(", ", filteredTags)}");
+            }
+
+            parts.Add(
+                $"{"Traits".Translate().RawText}: {string.Join(", ", pawn.story.traits.allTraits.Select(t => t.LabelCap).ToArray())}"
+            );
+            
+            message.Reply(string.Join("⎮", parts.ToArray()).WithHeader("TabCharacter".Translate()));
+        }
+    }
+}
