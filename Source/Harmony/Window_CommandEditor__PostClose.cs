708d5ac33eee51a9089e58942a0ca1cb344a9424
SirRandoo <randomshovel@gmail.com>
2020-04-19 18:24:44 -0500

    Ensured the mod dumps commands when the user modifies one.

---
+++ Source/Harmony/Window_CommandEditor__PostClose.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using SirRandoo.ToolkitUtils.Utils;
+using TwitchToolkit.Windows;
+
+namespace SirRandoo.ToolkitUtils.Harmony
+{
+    [HarmonyPatch(typeof(Window_CommandEditor), "PostClose")]
+    public static class Window_CommandEditor__PostClose
+    {
+        [HarmonyPostfix]
+        public static void PostClose()
+        {
+            ShopExpansionHelper.DumpCommands();
+        }
+    }
+}
