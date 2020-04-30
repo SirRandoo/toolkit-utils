987c162f456803ff48e9fe2c871a8ad537a2d3ff
SirRandoo <randomshovel@gmail.com>
2020-04-30 13:41:31 -0500

    Finally got rid of that gross base patch.

---
+++ Source/Harmony/IncidentEditorPatches.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using TwitchToolkit.Incidents;
+using TwitchToolkit.Windows;
+
+namespace SirRandoo.ToolkitUtils.Harmony
+{
+    internal static class StoreIncidentWindowVariables
+    {
+        public static readonly ConstructorInfo OldClassConstructor =
+            typeof(StoreIncidentEditor).GetConstructor(new[] {typeof(StoreIncident)});
+
+        public static readonly ConstructorInfo NewClassConstructor =
+            typeof(Windows.StoreIncidentEditor).GetConstructor(new[] {typeof(StoreIncident)});
+
+        public static readonly Type OldClassType = typeof(StoreIncidentEditor);
+        public static readonly Type NewClassType = typeof(Windows.StoreIncidentEditor);
+    }
+
+    [HarmonyPatch(typeof(StoreIncidentsWindow), "DoRow")]
+    public static class StoreIncidentsWindowPatch
+    {
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Newobj
+                    && instruction.OperandIs(StoreIncidentWindowVariables.OldClassConstructor))
+                {
+                    instruction.operand = StoreIncidentWindowVariables.NewClassConstructor;
+                }
+                else if (instruction.opcode == OpCodes.Ldtoken
+                         && instruction.OperandIs(StoreIncidentWindowVariables.OldClassType))
+                {
+                    instruction.operand = StoreIncidentWindowVariables.NewClassType;
+                }
+
+                yield return instruction;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Window_Trackers), "DoWindowContents")]
+    public static class WindowTrackersPatch
+    {
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Newobj
+                    && instruction.OperandIs(StoreIncidentWindowVariables.OldClassConstructor))
+                {
+                    instruction.operand = StoreIncidentWindowVariables.NewClassConstructor;
+                }
+                else if (instruction.opcode == OpCodes.Ldtoken
+                         && instruction.OperandIs(StoreIncidentWindowVariables.OldClassType))
+                {
+                    instruction.operand = StoreIncidentWindowVariables.NewClassType;
+                }
+
+                yield return instruction;
+            }
+        }
+    }
+}
---
+++ Source/Windows/StoreIncidentEditor.cs
@@ -0,0 +1,81 @@
+﻿using TwitchToolkit.Incidents;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Windows
+{
+    public class StoreIncidentEditor : TwitchToolkit.Windows.StoreIncidentEditor
+    {
+        public StoreIncidentEditor(StoreIncident storeIncident) : base(storeIncident)
+        {
+        }
+
+        public override void PostOpen()
+        {
+            base.PostOpen();
+
+            if (storeIncidentVariables == null)
+            {
+                return;
+            }
+
+            if (storeIncidentVariables.defName.Equals("AddTrait") && TkSettings.UtilsNoticeAdd)
+            {
+                Find.WindowStack.Add(new NoticeWindow(storeIncidentVariables.defName));
+            }
+
+            if (storeIncidentVariables.defName.Equals("RemoveTrait") && TkSettings.UtilsNoticeRemove)
+            {
+                Find.WindowStack.Add(new NoticeWindow(storeIncidentVariables.defName));
+            }
+
+            if (storeIncidentVariables.defName.Equals("BuyPawn") && TkSettings.UtilsNoticePawn)
+            {
+                Find.WindowStack.Add(new NoticeWindow(storeIncidentVariables.defName));
+            }
+        }
+
+        /// <inheritdoc />
+        public override void PreClose()
+        {
+            base.PreClose();
+
+            if (storeIncidentVariables == null)
+            {
+                return;
+            }
+
+            if (storeIncidentVariables.cost <= 1)
+            {
+                return;
+            }
+
+            switch (storeIncidentVariables.defName)
+            {
+                case "AddTrait":
+                    foreach (var trait in TkUtils.ShopExpansion.Traits)
+                    {
+                        trait.AddPrice = storeIncidentVariables.cost;
+                    }
+
+                    storeIncidentVariables.cost = 1;
+                    return;
+                case "RemoveTrait":
+                    foreach (var trait in TkUtils.ShopExpansion.Traits)
+                    {
+                        trait.RemovePrice = storeIncidentVariables.cost;
+                    }
+
+                    storeIncidentVariables.cost = 1;
+                    return;
+                case "BuyPawn":
+                    foreach (var race in TkUtils.ShopExpansion.Races)
+                    {
+                        race.Price = storeIncidentVariables.cost;
+                    }
+
+                    storeIncidentVariables.cost = 1;
+                    return;
+            }
+        }
+    }
+}
