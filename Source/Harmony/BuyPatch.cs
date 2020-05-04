907e09797c4fc90ad2df1a544554047367bd114f
SirRandoo <randomshovel@gmail.com>
2020-05-04 16:50:48 -0500

    Patched Toolkit to support any shortcut commands for !buy.

---
+++ Source/Harmony/BuyPatch.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using SirRandoo.ToolkitUtils.Utils;
+using TwitchLib.Client.Models.Interfaces;
+using TwitchToolkit;
+using TwitchToolkit.Commands.ViewerCommands;
+using TwitchToolkit.Store;
+
+namespace SirRandoo.ToolkitUtils.Harmony
+{
+    [HarmonyPatch(typeof(Buy), "RunCommand")]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class BuyPatch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(CommandDriver __instance, ITwitchMessage twitchMessage)
+        {
+            if (__instance == null)
+            {
+                return true;
+            }
+
+            var viewer = Viewers.GetViewer(twitchMessage.Username);
+            var message = twitchMessage;
+
+            if (!__instance.command.defName.Equals("Buy"))
+            {
+                message = twitchMessage.WithMessage($"!buy {twitchMessage.Message.Substring(1)}");
+            }
+
+            if (message.Message.Split(' ').Length < 2)
+            {
+                return false;
+            }
+
+            TkLogger.Info($"{twitchMessage.Message} → {message.Message}");
+            Purchase_Handler.ResolvePurchase(viewer, message);
+
+            return false;
+        }
+    }
+}
--- Source/Utils/MessageHelper.cs
+++ Source/Utils/MessageHelper.cs
@@ -1,4 +1,5 @@
 ﻿using TwitchLib.Client.Interfaces;
+using TwitchLib.Client.Models;
 using TwitchLib.Client.Models.Interfaces;
 using Verse;
 
@@ -10,7 +11,7 @@ namespace SirRandoo.ToolkitUtils.Utils
         {
             ReplyToUser(m.Username, message);
         }
-        
+
         internal static void Reply(this ITwitchMessage m, string message)
         {
             ReplyToUser(m.Username, message);
@@ -36,6 +37,61 @@ namespace SirRandoo.ToolkitUtils.Utils
             alt ??= $"{emoji}.Text";
 
             return TkSettings.Emojis ? emoji : alt;
+        }
+
+        internal static ITwitchMessage WithMessage(this ITwitchMessage m, string message)
+        {
+            if (m.WhisperMessage != null)
+            {
+                return new WhisperMessage(
+                    m.WhisperMessage.Badges,
+                    m.WhisperMessage.ColorHex,
+                    m.WhisperMessage.Color,
+                    m.WhisperMessage.Username,
+                    m.WhisperMessage.DisplayName,
+                    m.WhisperMessage.EmoteSet,
+                    m.WhisperMessage.ThreadId,
+                    m.WhisperMessage.MessageId,
+                    m.WhisperMessage.UserId,
+                    m.WhisperMessage.IsTurbo,
+                    m.WhisperMessage.BotUsername,
+                    message,
+                    m.WhisperMessage.UserType
+                );
+            }
+
+            if (m.ChatMessage != null)
+            {
+                return new ChatMessage(
+                    m.ChatMessage.BotUsername,
+                    m.ChatMessage.UserId,
+                    m.ChatMessage.Username,
+                    m.ChatMessage.DisplayName,
+                    m.ChatMessage.ColorHex,
+                    m.ChatMessage.Color,
+                    m.ChatMessage.EmoteSet,
+                    message,
+                    m.ChatMessage.UserType,
+                    m.ChatMessage.Channel,
+                    m.ChatMessage.Id,
+                    m.ChatMessage.IsSubscriber,
+                    m.ChatMessage.SubscribedMonthCount,
+                    m.ChatMessage.RoomId,
+                    m.ChatMessage.IsTurbo,
+                    m.ChatMessage.IsModerator,
+                    m.ChatMessage.IsMe,
+                    m.ChatMessage.IsBroadcaster,
+                    m.ChatMessage.Noisy,
+                    m.ChatMessage.RawIrcMessage,
+                    m.ChatMessage.EmoteReplacedMessage,
+                    m.ChatMessage.Badges,
+                    m.ChatMessage.CheerBadge,
+                    m.ChatMessage.Bits,
+                    m.ChatMessage.BitsInDollars
+                );
+            }
+
+            return null;
         }
     }
 }
