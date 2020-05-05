a46a7102fa98b472658abe301bea845143fa54b0
SirRandoo <randomshovel@gmail.com>
2020-05-05 17:07:07 -0500

    Changed how the mod overwrites Toolkit's store to be when it saves incidents.

---
+++ Source/Harmony/StoreIncidentEditorPatch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using TwitchToolkit.Incidents;
+using TwitchToolkit.Store;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Harmony
+{
+    [HarmonyPatch(typeof(Store_IncidentEditor), "UpdatePriceSheet")]
+    public static class StoreIncidentEditorPatch
+    {
+        [HarmonyPrefix]
+        public static void Prefix()
+        {
+            var incidents = DefDatabase<StoreIncident>.AllDefsListForReading;
+
+            foreach (var incident in incidents)
+            {
+                switch (incident.defName)
+                {
+                    case "BuyPawn":
+                    case "AddTrait":
+                    case "RemoveTrait":
+                        incident.cost = 1;
+                        return;
+                }
+            }
+        }
+    }
+}
