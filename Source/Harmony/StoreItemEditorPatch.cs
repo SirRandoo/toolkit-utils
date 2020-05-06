8236aa40588ac7cc13941ebd14c34cece0a52c56
SirRandoo <randomshovel@gmail.com>
2020-05-06 00:10:23 -0500

    Added a patch to automatically remove races from Toolkit's item store before it saves to disk.

---
+++ Source/Harmony/StoreItemEditorPatch.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using HarmonyLib;
+using TwitchToolkit.Store;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Harmony
+{
+    [HarmonyPatch(typeof(Store_ItemEditor), "UpdateStoreItemList")]
+    public class StoreItemEditorPatch
+    {
+        [HarmonyPrefix]
+        public static void Prefix()
+        {
+            var inventory = StoreInventory.items;
+
+            foreach (var item in inventory
+                .Where(item => item.price > 0)
+                .Where(item => TkUtils.ShopExpansion.Races.Any(r => r.DefName.Equals(item.defname))))
+            {
+                item.price = -10;
+            }
+        }
+    }
+}
