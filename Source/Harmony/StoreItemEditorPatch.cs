6a7bb84538c6ef8243c17508f4d119727b6c2c8f
SirRandoo <randomshovel@gmail.com>
2020-05-08 18:11:41 -0500

    Added a null check to ensure the race removal patch doesn't crash if it's run before the shop loads.

--- Source/Harmony/StoreItemEditorPatch.cs
+++ Source/Harmony/StoreItemEditorPatch.cs
@@ -16,12 +16,15 @@ namespace SirRandoo.ToolkitUtils.Harmony
             var inventory = StoreInventory.items;
             var tradeables = StoreDialog.GetTradeables().ToHashSet();
 
-            foreach (var item in inventory
-                .Where(item => !item.defname.NullOrEmpty())
-                .Where(item => item.price >= 0)
-                .Where(item => TkUtils.ShopExpansion.Races.Any(r => r.DefName.Equals(item.defname))))
+            if (TkUtils.ShopExpansion.Races != null)
             {
-                item.price = -10;
+                foreach (var item in inventory
+                    .Where(item => !item.defname.NullOrEmpty())
+                    .Where(item => item.price >= 0)
+                    .Where(item => TkUtils.ShopExpansion.Races.Any(r => r.DefName.Equals(item.defname))))
+                {
+                    item.price = -10;
+                }
             }
 
             StoreInventory.items = inventory
