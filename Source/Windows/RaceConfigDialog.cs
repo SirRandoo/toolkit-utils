26117574ae60d847fe490608793ef19154d4e628
SirRandoo <randomshovel@gmail.com>
2020-05-09 06:19:09 -0500

    Mild touch up to the race config.

--- Source/Windows/RaceConfigDialog.cs
+++ Source/Windows/RaceConfigDialog.cs
@@ -6,21 +6,48 @@ using Verse;
 
 namespace SirRandoo.ToolkitUtils.Windows
 {
+    [StaticConstructorOnStartup]
     public class RaceConfigDialog : Window
     {
+        private static readonly Texture2D SortingAscend;
+        private static readonly Texture2D SortingDescend;
         private readonly List<XmlRace> cache = TkUtils.ShopExpansion.Races;
+        private TaggedString applyText;
+
+        private bool control;
         private string currentQuery = "";
+        private TaggedString disableText;
+        private TaggedString enableText;
         private int globalCost;
         private string lastQuery = "";
-        private IReadOnlyCollection<XmlRace> results;
+        private TaggedString nameHeaderText;
+        private TaggedString priceHeaderText;
+        private TaggedString priceText;
+        private TaggedString resetText;
+        private List<XmlRace> results;
         private Vector2 scrollPos = Vector2.zero;
+        private TaggedString searchText;
+        private bool shift;
+
+        private Sorter sorter = Sorter.Name;
+        private SortMode sortMode = SortMode.Ascending;
+
+        private TaggedString titleText;
+
+        static RaceConfigDialog()
+        {
+            SortingAscend = ContentFinder<Texture2D>.Get("UI/Icons/Sorting");
+            SortingDescend = ContentFinder<Texture2D>.Get("UI/Icons/SortingDescending");
+        }
 
         public RaceConfigDialog()
         {
+            GetTranslations();
+
             doCloseX = true;
             forcePause = true;
 
-            optionalTitle = "TKUtils.Windows.Races.Title".Translate();
+            optionalTitle = titleText;
             cache?.SortBy(r => r.Name.NullOrEmpty() ? r.DefName : r.Name);
 
             if (cache == null)
@@ -31,14 +58,29 @@ namespace SirRandoo.ToolkitUtils.Windows
 
         public override Vector2 InitialSize => new Vector2(640f, 740f);
 
+        private void GetTranslations()
+        {
+            titleText = "TKUtils.Windows.Races.Title".Translate();
+            nameHeaderText = "TKUtils.Windows.Store.Headers.Name".Translate();
+            priceText = "TKUtils.Windows.Config.Input.Price.Label".Translate();
+            priceHeaderText = "TKUtils.Windows.Store.Headers.Price".Translate();
+            applyText = "TKUtils.Windows.Config.Buttons.Apply.Label".Translate();
+            searchText = "TKUtils.Windows.Config.Buttons.Search.Label".Translate();
+            resetText = "TKUtils.Windows.Config.Buttons.ResetAll.Label".Translate();
+            enableText = "TKUtils.Windows.Config.Buttons.EnableAll.Label".Translate();
+            disableText = "TKUtils.Windows.Config.Buttons.DisableAll.Label".Translate();
+        }
+
         private void DrawHeader(Rect inRect)
         {
+            GUI.BeginGroup(inRect);
+
             var midpoint = inRect.width / 2f;
             var searchRect = new Rect(inRect.x, inRect.y, inRect.width * 0.3f, Text.LineHeight);
             var searchLabel = searchRect.LeftHalf();
             var searchField = searchRect.RightHalf();
 
-            Widgets.Label(searchLabel, "TKUtils.Windows.Config.Buttons.Search.Label".Translate());
+            Widgets.Label(searchLabel, searchText);
             currentQuery = Widgets.TextField(searchField, currentQuery);
 
             if (currentQuery.Length > 0 && SettingsHelper.DrawClearButton(searchField))
@@ -53,14 +95,32 @@ namespace SirRandoo.ToolkitUtils.Windows
             }
 
 
-            var enableText = "TKUtils.Windows.Config.Buttons.EnableAll.Label".Translate();
-            var disableText = "TKUtils.Windows.Config.Buttons.DisableAll.Label".Translate();
-            var applyText = "TKUtils.Windows.Config.Buttons.Apply.Label".Translate();
-            var enableSize = Text.CalcSize(enableText) * 1.5f;
-            var disableSize = Text.CalcSize(disableText) * 1.5f;
-            var maxWidth = Mathf.Max(enableSize.x, disableSize.x);
-            var disableRect = new Rect(midpoint, inRect.y + Text.LineHeight, maxWidth, Text.LineHeight);
-            var enableRect = new Rect(midpoint, inRect.y, maxWidth, Text.LineHeight);
+            var maxWidth = Mathf.Max(
+                               Text.CalcSize(enableText).x,
+                               Text.CalcSize(disableText).x,
+                               Text.CalcSize(resetText).x
+                           )
+                           * 1.5f;
+            var disableRect = new Rect(inRect.width - maxWidth, inRect.y + Text.LineHeight, maxWidth, Text.LineHeight);
+            var enableRect = new Rect(inRect.width - maxWidth, inRect.y, maxWidth, Text.LineHeight);
+            var resetRect = new Rect(inRect.width - maxWidth * 2f, inRect.y, maxWidth, Text.LineHeight);
+
+            if (Widgets.ButtonText(resetRect, resetText))
+            {
+                var kinds = DefDatabase<PawnKindDef>.AllDefsListForReading;
+
+                foreach (var race in results ?? cache)
+                {
+                    var kind = kinds.FirstOrDefault(k => k.race?.defName.Equals(race.DefName) ?? false);
+
+                    if (kind == null)
+                    {
+                        continue;
+                    }
+
+                    race.Price = StoreDialog.CalculateToolkitPrice(kind.race?.BaseMarketValue ?? 0f);
+                }
+            }
 
             if (Widgets.ButtonText(enableRect, globalCost > 0 ? applyText : enableText))
             {
@@ -99,60 +159,123 @@ namespace SirRandoo.ToolkitUtils.Windows
             );
 
             var globalAddBuffer = globalCost.ToString();
-            Widgets.Label(globalCostRect.LeftHalf(), "TKUtils.Windows.Config.Input.Price.Label".Translate());
+            Widgets.Label(globalCostRect.LeftHalf(), priceText);
             Widgets.TextFieldNumeric(globalCostRect.RightHalf(), ref globalCost, ref globalAddBuffer);
 
             if (globalCost > 0 && SettingsHelper.DrawClearButton(globalCostRect.RightHalf()))
             {
                 globalCost = 0;
             }
+
+            GUI.EndGroup();
         }
 
         public override void DoWindowContents(Rect inRect)
         {
+            GUI.BeginGroup(inRect);
+
             var listing = new Listing_Standard {maxOneColumn = true};
 
-            DrawHeader(inRect);
+            DrawHeader(new Rect(0f, 0f, inRect.width, Text.LineHeight * 2f));
+
             Widgets.DrawLineHorizontal(inRect.x, Text.LineHeight * 3f, inRect.width);
+
+            var headingRect = new Rect(0f, Text.LineHeight * 4f, inRect.width, Text.LineHeight);
+            var nameHeadingRect = new Rect(0f, 0f, inRect.width * 0.5f, Text.LineHeight);
+            var priceHeadingRect = new Rect(
+                nameHeadingRect.x + nameHeadingRect.width + Widgets.CheckboxSize + 10f,
+                0f,
+                inRect.width - nameHeadingRect.width - Widgets.CheckboxSize - 26f,
+                Text.LineHeight
+            );
+
+            GUI.BeginGroup(headingRect);
+            Widgets.DrawHighlightIfMouseover(nameHeadingRect);
+            Widgets.DrawHighlightIfMouseover(priceHeadingRect);
+
+            if (Widgets.ButtonText(nameHeadingRect, "  " + nameHeaderText, false))
+            {
+                if (sorter != Sorter.Name)
+                {
+                    sortMode = SortMode.Descending;
+                }
+
+                sorter = Sorter.Name;
+                sortMode = sortMode == SortMode.Ascending ? SortMode.Descending : SortMode.Ascending;
+                SortCurrentWorkingList();
+            }
+
+            if (Widgets.ButtonText(priceHeadingRect, "  " + priceHeaderText, false))
+            {
+                if (sorter != Sorter.Price)
+                {
+                    sortMode = SortMode.Descending;
+                }
+
+                sorter = Sorter.Price;
+                sortMode = sortMode == SortMode.Ascending ? SortMode.Descending : SortMode.Ascending;
+
+                SortCurrentWorkingList();
+            }
+
+            switch (sorter)
+            {
+                case Sorter.Name:
+                    GUI.DrawTexture(
+                        new Rect(nameHeadingRect.x, nameHeadingRect.y + (Text.LineHeight / 2f) - 4f, 8f, 8f),
+                        sortMode != SortMode.Descending ? SortingAscend : SortingDescend
+                    );
+                    break;
+
+                case Sorter.Price:
+                    GUI.DrawTexture(
+                        new Rect(priceHeadingRect.x, priceHeadingRect.y + (Text.LineHeight / 2f) - 4f, 8f, 8f),
+                        sortMode != SortMode.Descending ? SortingAscend : SortingDescend
+                    );
+                    break;
+            }
+
+            GUI.EndGroup();
 
             var contentArea = new Rect(
                 inRect.x,
-                Text.LineHeight * 4f,
+                Text.LineHeight * 5f,
                 inRect.width,
-                inRect.height - Text.LineHeight * 3f
+                inRect.height - Text.LineHeight * 5f
             );
 
             var total = results?.Count ?? cache.Count;
-            var viewPort = new Rect(contentArea.x, 0f, contentArea.width * 0.9f, Text.LineHeight * total + 1);
+            var viewPort = new Rect(0f, 0f, contentArea.width - 16f, Text.LineHeight * total);
+            var races = new Rect(0f, 0f, contentArea.width, contentArea.height);
 
-            listing.BeginScrollView(contentArea, ref scrollPos, ref viewPort);
+            GUI.BeginGroup(contentArea);
+            listing.BeginScrollView(races, ref scrollPos, ref viewPort);
             foreach (var race in results ?? cache)
             {
                 var lineRect = listing.GetRect(Text.LineHeight);
-                var boxRect = new Rect(contentArea.x, lineRect.y, lineRect.width * 0.84f, lineRect.height);
-                Widgets.CheckboxLabeled(
-                    race.Enabled ? boxRect : lineRect,
-                    race.Name,
-                    ref race.Enabled
+                var nameRect = new Rect(0f, lineRect.y, nameHeadingRect.width, lineRect.height);
+                var stateRect = new Rect(
+                    nameHeadingRect.x + nameHeadingRect.width + 5f,
+                    lineRect.y,
+                    Widgets.CheckboxSize,
+                    Text.LineHeight
                 );
+                var priceRect = new Rect(priceHeadingRect.x, lineRect.y, priceHeadingRect.width - 16f, lineRect.height);
+
+                Widgets.Label(nameRect, race.Name);
+                Widgets.Checkbox(stateRect.x, stateRect.y, ref race.Enabled, paintable: true);
 
                 if (!race.Enabled)
                 {
                     continue;
                 }
 
-                var inputRect = new Rect(
-                    lineRect.width - contentArea.width * 0.14f,
-                    lineRect.y,
-                    contentArea.width * 0.14f,
-                    lineRect.height
-                );
-
-                var valueBuffer = race.Price.ToString();
-                Widgets.TextFieldNumeric(inputRect, ref race.Price, ref valueBuffer);
+                SettingsHelper.DrawPriceField(priceRect, ref race.Price, ref control, ref shift);
             }
 
             listing.EndScrollView(ref viewPort);
+
+            GUI.EndGroup();
         }
 
         private void Notify__SearchRequested()
@@ -160,6 +283,7 @@ namespace SirRandoo.ToolkitUtils.Windows
             lastQuery = currentQuery;
 
             results = GetSearchResults();
+            SortCurrentWorkingList();
         }
 
         public override void WindowUpdate()
@@ -177,7 +301,7 @@ namespace SirRandoo.ToolkitUtils.Windows
             }
         }
 
-        private IReadOnlyCollection<XmlRace> GetSearchResults()
+        private List<XmlRace> GetSearchResults()
         {
             var serialized = currentQuery?.ToToolkit();
 
@@ -202,6 +326,45 @@ namespace SirRandoo.ToolkitUtils.Windows
             if (TkSettings.JsonShop)
             {
                 ShopExpansionHelper.DumpShopExpansion();
+            }
+        }
+
+        private void SortCurrentWorkingList()
+        {
+            var workingList = results ?? cache;
+
+            switch (sorter)
+            {
+                case Sorter.Name:
+                    switch (sortMode)
+                    {
+                        case SortMode.Ascending:
+                            workingList.SortBy(i => i.Name);
+                            results = workingList;
+                            return;
+                        case SortMode.Descending:
+                            workingList.SortByDescending(i => i.Name);
+                            results = workingList;
+                            return;
+                        default:
+                            return;
+                    }
+                case Sorter.Price:
+                    switch (sortMode)
+                    {
+                        case SortMode.Ascending:
+                            workingList.SortBy(i => i.Price);
+                            results = workingList;
+                            return;
+                        case SortMode.Descending:
+                            workingList.SortByDescending(i => i.Price);
+                            results = workingList;
+                            return;
+                        default:
+                            return;
+                    }
+                default:
+                    return;
             }
         }
     }
