42937726275b1222c6717db863c092b9d4390ddc
SirRandoo <randomshovel@gmail.com>
2020-05-19 16:37:04 -0500

    Added the ability to disable the hair color changing code from Toolkit, and fixed the color code being improperly parsed.

---
+++ Source/Harmony/ViewerUpdaterPatch.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using TwitchLib.Client.Models.Interfaces;
+using TwitchToolkit;
+using TwitchToolkit.PawnQueue;
+using TwitchToolkit.Twitch;
+using UnityEngine;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Harmony
+{
+    [HarmonyPatch(typeof(ViewerUpdater), "ParseMessage")]
+    public class ViewerUpdaterPatch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ITwitchMessage twitchMessage)
+        {
+            if (twitchMessage?.ChatMessage == null)
+            {
+                return false;
+            }
+
+            var viewer = Viewers.GetViewer(twitchMessage.Username);
+            var component = Current.Game.GetComponent<GameComponentPawns>();
+
+            ToolkitSettings.ViewerColorCodes[twitchMessage.Username.ToLowerInvariant()] =
+                twitchMessage.ChatMessage.ColorHex;
+
+            if (component.HasUserBeenNamed(twitchMessage.Username) && TkSettings.HairColor)
+            {
+                if (ColorUtility.TryParseHtmlString(twitchMessage.ChatMessage.ColorHex, out var hairColor))
+                {
+                    component.PawnAssignedToUser(twitchMessage.Username).story.hairColor = hairColor;
+                }
+            }
+
+            if (twitchMessage.ChatMessage.IsModerator && !viewer.mod)
+            {
+                viewer.SetAsModerator();
+            }
+
+            if (twitchMessage.ChatMessage.IsSubscriber && !viewer.IsSub)
+            {
+                viewer.subscriber = true;
+            }
+
+            if (!twitchMessage.ChatMessage.IsVip || viewer.IsVIP)
+            {
+                return false;
+            }
+
+            viewer.vip = true;
+
+            return false;
+        }
+    }
+}
