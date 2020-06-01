ace727b7634ac4db560c2a49b250548a535d9319
SirRandoo <randomshovel@gmail.com>
2020-06-01 10:31:00 -0500

    Added support for Immortals.

---
+++ Source/IncidentHelpers/ImmortalityHelper.cs
@@ -0,0 +1,60 @@
+﻿using RimWorld;
+using SirRandoo.ToolkitUtils.Utils;
+using SirRandoo.ToolkitUtils.Utils.ModComp;
+using TwitchToolkit;
+using TwitchToolkit.Store;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.IncidentHelpers
+{
+    public class ImmortalityHelper : IncidentHelperVariables
+    {
+        private Pawn pawn;
+
+        public override Viewer Viewer { get; set; }
+
+        public override bool IsPossible(string message, Viewer viewer, bool separateChannel = false)
+        {
+            Viewer = viewer;
+            pawn = CommandBase.GetOrFindPawn(Viewer.username);
+
+            if (pawn == null)
+            {
+                MessageHelper.ReplyToUser(Viewer.username, $"TKUtils.Responses.NoPawn".Translate());
+                return false;
+            }
+
+            if (!Immortals.Active)
+            {
+                return false;
+            }
+
+            return !pawn.health.hediffSet.HasHediff(Immortals.ImmortalHediffDef);
+        }
+
+        public override void TryExecute()
+        {
+            if (!Immortals.TryGrantImmortality(pawn))
+            {
+                return;
+            }
+
+            if (!ToolkitSettings.UnlimitedCoins)
+            {
+                Viewer.TakeViewerCoins(storeIncident.cost);
+            }
+
+            if (ToolkitSettings.PurchaseConfirmations)
+            {
+                MessageHelper.ReplyToUser(Viewer.username, "TKUtils.Responses.Immortality".Translate());
+            }
+
+            Find.LetterStack.ReceiveLetter(
+                "TKUtils.Letters.Immortality.Title".Translate(),
+                "TKUtils.Letters.Immortality.Description".Translate(Viewer.username),
+                LetterDefOf.NeutralEvent,
+                pawn
+            );
+        }
+    }
+}
---
+++ Source/Utils/ModComp/Immortals.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Utils.ModComp
+{
+    [StaticConstructorOnStartup]
+    public static class Immortals
+    {
+        public static readonly bool Active;
+        internal static readonly HediffDef ImmortalHediffDef;
+
+        static Immortals()
+        {
+            foreach (Mod _ in LoadedModManager.ModHandles.Where(
+                h => h.Content.PackageId.EqualsIgnoreCase("fridgeBaron.Immortals")
+            ))
+            {
+                try
+                {
+                    ImmortalHediffDef = DefDatabase<HediffDef>.AllDefsListForReading
+                        .FirstOrDefault(h => h.defName.Equals("IH_Immortal"));
+
+                    Active = ImmortalHediffDef != null;
+                }
+                catch (Exception e)
+                {
+                    TkLogger.Error("Compatibility class for Immortals failed!", e);
+                }
+            }
+        }
+
+        public static bool TryGrantImmortality(Pawn pawn)
+        {
+            try
+            {
+                pawn.health.AddHediff(ImmortalHediffDef);
+                return true;
+            }
+            catch (Exception e)
+            {
+                TkLogger.Error($"Could not grant immortality to {pawn.LabelCap}", e);
+                return false;
+            }
+        }
+    }
+}
