2fb2cb500a4d2bb3b15da10bd7c6e9dd84bbd53e
SirRandoo <randomshovel@gmail.com>
2020-06-02 00:28:07 -0500

    Added an exception handler to Toolkit's purchase handler.

---
+++ Source/Harmony/PurchaseHandlerPatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using TwitchToolkit.Store;
+
+namespace SirRandoo.ToolkitUtils.Harmony
+{
+    [HarmonyPatch(typeof(Purchase_Handler), "ResolvePurchase")]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class PurchaseHandlerPatch
+    {
+        [HarmonyFinalizer]
+        public static Exception Finalizer(Exception __exception)
+        {
+            if (__exception != null)
+            {
+                TkLogger.Error("Purchase handler raised an exception!", __exception);
+            }
+
+            return null;
+        }
+    }
+}
