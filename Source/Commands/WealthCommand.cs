5c206f369215aeddab110cb392de74461ec1290c
SirRandoo <randomshovel@gmail.com>
2020-06-16 04:42:22 -0500

    Added a wealth command.

---
+++ Source/Commands/WealthCommand.cs
@@ -0,0 +1,24 @@
+﻿using SirRandoo.ToolkitUtils.Utils;
+using TwitchLib.Client.Models.Interfaces;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Commands
+{
+    public class WealthCommand : CommandBase
+    {
+        public override void RunCommand(ITwitchMessage twitchMessage)
+        {
+            if (Current.Game?.CurrentMap == null)
+            {
+                return;
+            }
+
+            twitchMessage.Reply(
+                "TKUtils.Formats.KeyValue".Translate(
+                    "TKUtils.Responses.Wealth".TranslateSimple(),
+                    Current.Game.CurrentMap.PlayerWealthForStoryteller.ToString("N0")
+                )
+            );
+        }
+    }
+}
