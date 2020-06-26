766e165b3fe0fbef6e6622c5dabaac6498915195
SirRandoo <randomshovel@gmail.com>
2020-06-26 07:58:31 -0500

    Moved Utils' Toolkit window to the windows namespace, and added a mod extension for incidents.

---
+++ Source/EventExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils
+{
+    public enum EventTypes
+    {
+        Void, Trait, Race,
+        Item
+    }
+
+    public class EventExtension : DefModExtension
+    {
+        [Description("A brief explanation of what the event does.")]
+        [DefaultValue(null)]
+        public string Description;
+
+        [Description("The product category this event's final price will be.")]
+        [DefaultValue(EventTypes.Void)]
+        public EventTypes EventType;
+
+        [Description("The parameters this event can take.")]
+        [DefaultValue(null)]
+        public List<Parameter> Parameters;
+    }
+}
