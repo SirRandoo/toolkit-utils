6a7d434c19624af6d7dce294caa70795744518a7
SirRandoo <randomshovel@gmail.com>
2020-06-29 18:07:34 -0500

    Added new textures to Utils.

--- Source/Textures.cs
+++ Source/Textures.cs
@@ -4,13 +4,19 @@ using Verse;
 namespace SirRandoo.ToolkitUtils
 {
     [StaticConstructorOnStartup]
-    public class Textures
+    public static class Textures
     {
         internal static readonly Texture2D SortingAscend;
         internal static readonly Texture2D SortingDescend;
+        internal static readonly Texture2D Gear;
+        internal static readonly Texture2D Trash;
+        internal static readonly Texture2D Refresh;
 
         static Textures()
         {
+            Gear = ContentFinder<Texture2D>.Get("UI/Icons/Gear");
+            Trash = ContentFinder<Texture2D>.Get("UI/Icons/Trash");
+            Refresh = ContentFinder<Texture2D>.Get("UI/Icons/Refresh");
             SortingAscend = ContentFinder<Texture2D>.Get("UI/Icons/Sorting");
             SortingDescend = ContentFinder<Texture2D>.Get("UI/Icons/SortingDescending");
         }
