f4109ccdc5ec24b958724fa8103a55fb1afe342f
SirRandoo <randomshovel@gmail.com>
2020-07-20 19:38:15 -0500

    Committed the missing using statement in StoreDialog.cs.

--- Source/Windows/StoreDialog.cs
+++ Source/Windows/StoreDialog.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using SirRandoo.ToolkitUtils.Helpers;
+using SirRandoo.ToolkitUtils.Models;
 using TwitchToolkit.Store;
 using UnityEngine;
 using Verse;
