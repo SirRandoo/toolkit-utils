e821be3c614acd92b399b70909a96510938756dc
SirRandoo <randomshovel@gmail.com>
2020-07-25 14:13:04 -0500

    Fixed the legacy shop file path being incorrect.

--- Source/Paths.cs
+++ Source/Paths.cs
@@ -7,7 +7,7 @@ namespace SirRandoo.ToolkitUtils
     {
         public static readonly string TraitFilePath = Path.Combine(SaveHelper.dataPath, "traits.json");
         public static readonly string ModListFilePath = Path.Combine(SaveHelper.dataPath, "modlist.json");
-        public static readonly string LegacyShopFilePath = Path.Combine(SaveHelper.dataPath, "ShopExt.xml");
+        public static readonly string LegacyShopFilePath = Path.Combine(SaveHelper.dataPath, "ShopExt_1.xml");
         public static readonly string ItemDataFilePath = Path.Combine(SaveHelper.dataPath, "itemdata.json");
         public static readonly string PawnKindFilePath = Path.Combine(SaveHelper.dataPath, "pawnkinds.json");
         public static readonly string CommandListFilePath = Path.Combine(SaveHelper.dataPath, "commands.json");
