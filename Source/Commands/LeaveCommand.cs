af9cdc0fa48ad0b3d2e7dd44c79973990dfaf96b
SirRandoo <randomshovel@gmail.com>
2020-08-14 07:34:19 -0500

    Made the leave command only unassign undead pawns instead of nulling their faction.

--- Source/Commands/LeaveCommand.cs
+++ Source/Commands/LeaveCommand.cs
@@ -3,6 +3,7 @@ using RimWorld;
 using RimWorld.Planet;
 using SirRandoo.ToolkitUtils.Helpers;
 using SirRandoo.ToolkitUtils.Utils;
+using SirRandoo.ToolkitUtils.Utils.ModComp;
 using TwitchLib.Client.Models.Interfaces;
 using TwitchToolkit.PawnQueue;
 using UnityEngine;
@@ -26,6 +27,16 @@ namespace SirRandoo.ToolkitUtils.Commands
             if (pawn.IsCaravanMember())
             {
                 twitchMessage.Reply("TKUtils.Leave.Caravan".Localize());
+                return;
+            }
+
+            GameComponentPawns component;
+            if (MagicComp.Active && pawn.IsUndead())
+            {
+                twitchMessage.Reply("TKUtils.Leave.Undead".Localize());
+
+                component = Current.Game.GetComponent<GameComponentPawns>();
+                component?.pawnHistory.Remove(twitchMessage.Username);
                 return;
             }
 
@@ -71,7 +82,7 @@ namespace SirRandoo.ToolkitUtils.Commands
                 pawn.health.surgeryBills.Clear();
             }
 
-            var component = Current.Game.GetComponent<GameComponentPawns>();
+            component = Current.Game.GetComponent<GameComponentPawns>();
             component?.pawnHistory.Remove(twitchMessage.Username);
         }
     }
