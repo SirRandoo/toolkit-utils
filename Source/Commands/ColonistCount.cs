c3ce57d2bc74224f818dc7e2c1765b31d48b56ad
SirRandoo <randomshovel@gmail.com>
2020-08-26 13:15:38 -0500

    Added a colonist count command.

---
+++ Source/Commands/ColonistCount.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using SirRandoo.ToolkitUtils.Helpers;
+using SirRandoo.ToolkitUtils.Utils;
+using TwitchLib.Client.Models.Interfaces;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Commands
+{
+    [UsedImplicitly]
+    public class ColonistCount : CommandBase
+    {
+        public override void RunCommand(ITwitchMessage twitchMessage)
+        {
+            if (Find.ColonistBar == null)
+            {
+                return;
+            }
+
+            List<Pawn> colonists = Find.ColonistBar.GetColonistsInOrder();
+
+            if (colonists.Count <= 0)
+            {
+                twitchMessage.Reply("TKUtils.ColonistCount.None".Localize());
+                return;
+            }
+
+            twitchMessage.Reply("TKUtils.ColonistCount.Any".Localize(colonists.Count.ToString("N0")));
+        }
+    }
+}
