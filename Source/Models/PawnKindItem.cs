6147020cac15790ad80dd6dc7506857c04f86b02
SirRandoo <randomshovel@gmail.com>
2020-09-09 13:53:56 -0500

    Cached kind defs inside of PawnKindItem.

--- Source/Models/PawnKindItem.cs
+++ Source/Models/PawnKindItem.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
 using Verse;
@@ -9,28 +10,35 @@ namespace SirRandoo.ToolkitUtils.Models
     public class PawnKindItem
     {
         [JsonProperty("price")] public int Cost;
-        [CanBeNull] public PawnKindData Data;
+        [JsonIgnore] private PawnKindData data;
+
         public string DefName;
         public bool Enabled;
+        [JsonIgnore] private List<PawnKindDef> kinds;
         public string Name;
+
+        [JsonIgnore]
+        public IEnumerable<PawnKindDef> Kinds =>
+            kinds ??= DefDatabase<PawnKindDef>.AllDefs.Where(k => k.race.defName.Equals(DefName)).ToList();
+
+        [JsonIgnore]
+        private PawnKindDef ColonistKindDef =>
+            Kinds.FirstOrDefault(k => k.defaultFactionType.isPlayer) ?? Kinds.FirstOrFallback();
+
+        public PawnKindData Data => data ??= new PawnKindData();
+
 
         public static PawnKindItem MigrateFrom(XmlRace race)
         {
             return new PawnKindItem
             {
-                DefName = race.DefName,
-                Enabled = race.Enabled,
-                Name = race.Name,
-                Cost = race.Price,
-                Data = new PawnKindData {CustomName = false}
+                DefName = race.DefName, Enabled = race.Enabled, Name = race.Name, Cost = race.Price
             };
         }
 
         public string GetDefaultName()
         {
-            PawnKindDef kindDef = DefDatabase<PawnKindDef>.AllDefs.FirstOrDefault(d => d.race.defName.Equals(DefName));
-
-            return kindDef?.label ?? DefName;
+            return ColonistKindDef?.label ?? DefName;
         }
     }
 }
