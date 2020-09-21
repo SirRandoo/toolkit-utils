815c2c29d1d6551635a9a6e55f5ebfe6b885889f
SirRandoo <randomshovel@gmail.com>
2020-09-20 21:45:53 -0500

    Added support for changing work priorities via the pawn work command.

--- Source/Commands/PawnWork.cs
+++ Source/Commands/PawnWork.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
+using RimWorld;
 using SirRandoo.ToolkitUtils.Helpers;
 using SirRandoo.ToolkitUtils.Utils;
 using TwitchLib.Client.Models.Interfaces;
+using UnityEngine;
 using Verse;
 
 namespace SirRandoo.ToolkitUtils.Commands
@@ -30,6 +32,44 @@ namespace SirRandoo.ToolkitUtils.Commands
                 return;
             }
 
+            List<KeyValuePair<string, string>> newPriorities = CommandParser.ParseKeyed(twitchMessage.Message);
+
+            if (!newPriorities.NullOrEmpty())
+            {
+                ProcessChangeRequests(pawn, newPriorities);
+            }
+
+            string summary = GetWorkPrioritySummary(pawn);
+
+            if (summary.NullOrEmpty())
+            {
+                return;
+            }
+
+            twitchMessage.Reply(summary.WithHeader("TKUtils.PawnWork.Header".Localize()));
+        }
+
+        private static void ProcessChangeRequests(Pawn pawn, List<KeyValuePair<string, string>> rawChanges)
+        {
+            List<WorkTypeDef> priorities = WorkTypeDefsUtility.WorkTypeDefsInPriorityOrder
+               .Where(w => !pawn.WorkTypeIsDisabled(w))
+               .ToList();
+
+            foreach (KeyValuePair<string, string> pair in rawChanges)
+            {
+                WorkTypeDef workTypeDef = priorities.FirstOrDefault(w => w.label.EqualsIgnoreCase(pair.Key));
+
+                if (workTypeDef == null || !int.TryParse(pair.Value, out int parsed))
+                {
+                    continue;
+                }
+
+                pawn.workSettings.SetPriority(workTypeDef, Mathf.Clamp(parsed, 0, Pawn_WorkSettings.LowestPriority));
+            }
+        }
+
+        private static string GetWorkPrioritySummary(Pawn pawn)
+        {
             List<WorkTypeDef> priorities = WorkTypeDefsUtility.WorkTypeDefsInPriorityOrder.ToList();
 
             if (TkSettings.SortWorkPriorities)
@@ -52,10 +92,7 @@ namespace SirRandoo.ToolkitUtils.Commands
                 )
                .ToList();
 
-            if (container.Count > 0)
-            {
-                twitchMessage.Reply(container.SectionJoin().WithHeader("TKUtils.PawnWork.Header".Localize()));
-            }
+            return container.Count > 0 ? container.SectionJoin() : null;
         }
 
         private static void HideDisabledWork(IList<WorkTypeDef> priorities)
