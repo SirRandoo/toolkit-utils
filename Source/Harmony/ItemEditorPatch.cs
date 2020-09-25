cb3f404f1a6f50e659fb8441cfa057266ac3ebe7
SirRandoo <randomshovel@gmail.com>
2020-09-25 07:52:49 -0500

    Added a check to ensure ToolkitUtils' item containers are regenerated if they're invalid whenever Toolkit saves its item data.

---
+++ Source/Harmony/ItemEditorPatch.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SirRandoo.ToolkitUtils.Windows;
+using TwitchToolkit.Store;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Harmony
+{
+    [UsedImplicitly(ImplicitUseKindFlags.Default, ImplicitUseTargetFlags.WithMembers)]
+    [HarmonyPatch(typeof(Store_ItemEditor), nameof(Store_ItemEditor.UpdateStoreItemList))]
+    public static class ItemEditorPatch
+    {
+        public static void Postfix()
+        {
+            if (!Data.Items.NullOrEmpty())
+            {
+                return;
+            }
+
+            TkLogger.Info("ToolkitUtils' item containers were null or empty; regenerating...");
+            Data.Items = StoreDialog.GenerateContainers().ToList();
+        }
+    }
+}
