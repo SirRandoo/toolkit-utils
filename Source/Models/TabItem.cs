4add2ace0d0c47938c717f128997dd979e3e257f
SirRandoo <randomshovel@gmail.com>
2020-10-15 00:07:25 -0500

    Included a tab worker implementation from another project.

---
+++ Source/Models/TabItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Models
+{
+    public class TabItem
+    {
+        private string label;
+
+        public string Label
+        {
+            get => label;
+            set
+            {
+                label = value;
+                Width = Text.CalcSize(label).x;
+            }
+        }
+
+        public float Width { get; private set; }
+
+        public Action<Rect> ContentDrawer { get; set; }
+        public Func<bool> Clicked { get; set; }
+
+        public void Draw(Rect canvas, float margins = 16f)
+        {
+            GUI.color = new Color(0.46f, 0.49f, 0.5f);
+            Widgets.DrawHighlight(canvas);
+            GUI.color = Color.white;
+
+            Rect transformedCanvas = canvas.ContractedBy(margins);
+            GUI.BeginGroup(transformedCanvas);
+            ContentDrawer(new Rect(0f, 0f, transformedCanvas.width, transformedCanvas.height));
+            GUI.EndGroup();
+        }
+    }
+}
---
+++ Source/Utils/TabWorker.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SirRandoo.ToolkitUtils.Helpers;
+using SirRandoo.ToolkitUtils.Models;
+using UnityEngine;
+
+namespace SirRandoo.ToolkitUtils.Utils
+{
+    public class TabWorker
+    {
+        private readonly List<TabItem> tabItems = new List<TabItem>();
+
+        public TabItem SelectedTab { get; set; }
+
+        public static TabWorker CreateInstance()
+        {
+            return new TabWorker();
+        }
+
+        public static TabWorker CreateInstance(IEnumerable<TabItem> tabs)
+        {
+            var worker = new TabWorker();
+            worker.tabItems.AddRange(tabs);
+
+            return worker;
+        }
+
+        public void AddTab(TabItem tab)
+        {
+            tabItems.Add(tab);
+        }
+
+        public void RemoveTab(string label)
+        {
+            TabItem tab = GetTab(label);
+
+            if (tab != null)
+            {
+                tabItems.Remove(tab);
+            }
+        }
+
+        public TabItem GetTab(string label)
+        {
+            return tabItems.FirstOrDefault(t => t.Label.Equals(label, StringComparison.InvariantCulture));
+        }
+
+        public void Draw(Rect region, bool vertical = false)
+        {
+            float offset = 0;
+            foreach (TabItem tab in tabItems)
+            {
+                var tabRegion = new Rect(region.x, region.y, tab.Width + 25f, region.height);
+
+                if (vertical)
+                {
+                    tabRegion.y += offset;
+                }
+                else
+                {
+                    tabRegion.x += offset;
+                }
+
+                if (SettingsHelper.DrawTabButton(
+                    tabRegion,
+                    tab.Label,
+                    TextAnchor.MiddleCenter,
+                    vertical: vertical,
+                    selected: SelectedTab == tab
+                ))
+                {
+                    if (tab.Clicked == null || tab.Clicked())
+                    {
+                        SelectedTab = tab;
+                    }
+                }
+
+                offset += tabRegion.width;
+            }
+        }
+    }
+}
