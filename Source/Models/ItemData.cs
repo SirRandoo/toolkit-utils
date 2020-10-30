a5d941dd007591d30827348e060a9e36da319280
SirRandoo <randomshovel@gmail.com>
2020-10-30 16:45:18 -0500

    Started the ground work for assigning research projects to items.

--- Source/Models/ItemData.cs
+++ Source/Models/ItemData.cs
@@ -1,22 +1,62 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.Linq;
 using JetBrains.Annotations;
+using Newtonsoft.Json;
 using TwitchToolkit;
+using Verse;
 
 namespace SirRandoo.ToolkitUtils.Models
 {
     [UsedImplicitly]
     public class ItemData
     {
-        [DefaultValue(null)] public string CustomName;
+        [System.ComponentModel.DefaultValue(null)]
+        public string CustomName;
 
         public bool HasQuantityLimit;
         public bool IsMelee;
         public bool IsRanged;
-        [DefaultValue(true)] public bool IsStuffAllowed;
+
+        [System.ComponentModel.DefaultValue(true)]
+        public bool IsStuffAllowed;
+
         public bool IsWeapon;
-        [DefaultValue(null)] public KarmaType? KarmaType;
+
+        [System.ComponentModel.DefaultValue(null)]
+        public KarmaType? KarmaType;
 
         public string Mod;
-        [DefaultValue(-1)] public int QuantityLimit;
+
+        [System.ComponentModel.DefaultValue(-1)]
+        public int QuantityLimit;
+
+        [JsonIgnore] private List<ResearchProjectDef> researchProjectDefs;
+
+        [System.ComponentModel.DefaultValue(null)]
+        public List<string> ResearchProjects;
+
+        public IEnumerable<ResearchProjectDef> GetResearchProjects()
+        {
+            if (ResearchProjects.NullOrEmpty() || researchProjectDefs.NullOrEmpty())
+            {
+                yield break;
+            }
+
+            if (researchProjectDefs.Count <= 0)
+            {
+                researchProjectDefs ??= new List<ResearchProjectDef>();
+                foreach (ResearchProjectDef projectDef in DefDatabase<ResearchProjectDef>.AllDefs.Where(
+                    r => ResearchProjects.Contains(r.defName)
+                ))
+                {
+                    researchProjectDefs.Add(projectDef);
+                }
+            }
+
+            foreach (ResearchProjectDef projectDef in researchProjectDefs)
+            {
+                yield return projectDef;
+            }
+        }
     }
 }
