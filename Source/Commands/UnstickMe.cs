e048cfe9f194513ead2718f18a3763c941383f19
SirRandoo <randomshovel@gmail.com>
2020-11-13 15:05:23 -0600

    Added a command that standard viewers can use to unstick themselves.

---
+++ Source/Commands/UnstickMe.cs
@@ -0,0 +1,32 @@
+﻿using JetBrains.Annotations;
+using SirRandoo.ToolkitUtils.Helpers;
+using SirRandoo.ToolkitUtils.Utils;
+using TwitchLib.Client.Models.Interfaces;
+using TwitchToolkit.Store;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Commands
+{
+    [UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature, ImplicitUseTargetFlags.WithMembers)]
+    public class UnstickMe : CommandBase
+    {
+        public override void RunCommand(ITwitchMessage twitchMessage)
+        {
+            if (!Purchase_Handler.viewerNamesDoingVariableCommands.Contains(twitchMessage.Username.ToLowerInvariant()))
+            {
+                return;
+            }
+
+            if (Find.TickManager?.Paused ?? true)
+            {
+                twitchMessage.Reply("TKUtils.Paused".Localize());
+                return;
+            }
+
+            if (Purchase_Handler.viewerNamesDoingVariableCommands.Remove(twitchMessage.Username.ToLowerInvariant()))
+            {
+                twitchMessage.Reply("TKUtils.UnstickMe".Localize());
+            }
+        }
+    }
+}
