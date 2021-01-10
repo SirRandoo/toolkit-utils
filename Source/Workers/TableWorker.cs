02ac5c8479e52b1f2f3df95a99e5faa1099c0de9
SirRandoo <randomshovel@gmail.com>
2021-01-10 13:03:51 -0600

    Implemented a common class for drawing table-style menus.

---
+++ Source/Workers/TableWorker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Workers
+{
+    public enum SortOrder { Ascending, Descending }
+
+    public abstract class TableWorker<T>
+    {
+        protected List<T> Data;
+
+        protected float LineHeight => Mathf.FloorToInt(Text.LineHeight);
+
+        protected float RowLineHeight => Mathf.FloorToInt(Text.LineHeight * 1.333f);
+
+        public void Draw(Rect canvas)
+        {
+            var headerRow = new Rect(canvas.x, canvas.y, canvas.width, LineHeight);
+            var tableContents = new Rect(
+                canvas.x,
+                canvas.y + headerRow.height,
+                canvas.width,
+                canvas.height - headerRow.height
+            );
+
+            GUI.BeginGroup(headerRow);
+            DrawHeaders(new Rect(0f, 0f, headerRow.width, headerRow.height));
+            GUI.EndGroup();
+
+            GUI.BeginGroup(tableContents);
+            DrawTableContents(new Rect(0f, 0f, tableContents.width, tableContents.height));
+            GUI.EndGroup();
+        }
+
+        public abstract void DrawHeaders(Rect canvas);
+        public abstract void DrawTableContents(Rect canvas);
+        public abstract void Prepare();
+
+        public abstract void NotifySortRequested();
+        public abstract void NotifySearchRequested(string query);
+        public abstract void NotifyResolutionChanged(Rect canvas);
+        protected abstract void FilterDataBySearch(string query);
+    }
+}
