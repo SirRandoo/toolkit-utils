a58bd0ee53e72ba053e0045a98dce9ab49cc729a
SirRandoo <randomshovel@gmail.com>
2021-02-22 02:51:42 -0600

    Fixed the item incidents not properly handling the quantity wildcard value.

--- Source/ToolkitUtils/Incidents/BuyItem.cs
+++ Source/ToolkitUtils/Incidents/BuyItem.cs
@@ -69,7 +69,7 @@ namespace SirRandoo.ToolkitUtils.Incidents
 
             if (product.Data != null && product.Data.HasQuantityLimit)
             {
-                amount = Mathf.Min(amount, product.Data.QuantityLimit);
+                amount = Mathf.Clamp(amount, 1, product.Data.QuantityLimit);
             }
 
             purchaseRequest = new PurchaseRequest
