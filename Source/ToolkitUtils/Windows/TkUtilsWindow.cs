53e732e04fe5f3f749ec88e78a535d181deeefc8
SirRandoo <randomshovel@gmail.com>
2021-02-22 03:41:06 -0600

    Added copyright file headers.

--- Source/ToolkitUtils/Windows/TkUtilsWindow.cs
+++ Source/ToolkitUtils/Windows/TkUtilsWindow.cs
@@ -1,4 +1,20 @@
-﻿using TwitchToolkit.Settings;
+﻿// ToolkitUtils
+// Copyright (C) 2021  SirRandoo
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using TwitchToolkit.Settings;
 using UnityEngine;
 using Verse;
 
@@ -13,7 +29,7 @@ namespace SirRandoo.ToolkitUtils.Windows
 
         public override void DoWindowContents(Rect inRect)
         {
-            TkSettings.DoWindowContents(
+            Mod.DoSettingsWindowContents(
                 new Rect(inRect.x, inRect.y, inRect.width, inRect.height - 40f - CloseButSize.y)
             );
         }
