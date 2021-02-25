5c4b105ae452ff166fc2d2546881f96e1226aac0
SirRandoo <randomshovel@gmail.com>
2021-02-25 15:01:16 -0600

    Shrunk the store dialog's width a bit.

--- Source/ToolkitUtils/Windows/StoreDialog.cs
+++ Source/ToolkitUtils/Windows/StoreDialog.cs
@@ -88,7 +88,7 @@ namespace SirRandoo.ToolkitUtils.Windows
             }
         }
 
-        public override Vector2 InitialSize => new Vector2(1024f, UI.screenHeight * 0.9f);
+        public override Vector2 InitialSize => new Vector2(900f, Mathf.FloorToInt(UI.screenHeight * 0.9f));
         protected override float Margin => 22f;
 
         private void NotifySearchRequested()
