a36290fab251255951408e6b5d7ab89a91aa3bd5
SirRandoo <randomshovel@gmail.com>
2021-03-08 14:28:53 -0600

    Added views to the newly written table workers.

---
+++ Source/ToolkitUtils/Workers/PawnViewWorker.cs
@@ -0,0 +1,63 @@
+﻿// ToolkitUtils
+// Copyright (C) 2021  SirRandoo
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using SirRandoo.ToolkitUtils.Helpers;
+using SirRandoo.ToolkitUtils.Models.Tables;
+using UnityEngine;
+
+namespace SirRandoo.ToolkitUtils.Workers
+{
+    public class PawnViewWorker : PawnTableWorker
+    {
+        public override void DrawHeaders(Rect canvas)
+        {
+            DrawSortableHeaders();
+            DrawSortableHeaderIcon();
+        }
+
+        protected override void DrawKind(Rect canvas, PawnTableItem item)
+        {
+            var nameRect = new Rect(NameHeaderTextRect.x, canvas.y, NameHeaderTextRect.width, RowLineHeight);
+            var priceRect = new Rect(PriceHeaderTextRect.x, canvas.y, PriceHeaderTextRect.width, RowLineHeight);
+
+            SettingsHelper.DrawLabel(nameRect, item.Data.Name);
+
+            if (item.Data.Enabled)
+            {
+                SettingsHelper.DrawLabel(priceRect, item.Data.Cost.ToString("N0"));
+            }
+        }
+
+        public override void NotifyResolutionChanged(Rect canvas)
+        {
+            float distributedWidth = Mathf.FloorToInt((canvas.width - 16f) * 0.3333f);
+            NameHeaderRect = new Rect(0f, 0f, distributedWidth, LineHeight);
+            NameHeaderTextRect = new Rect(
+                NameHeaderRect.x + 4f,
+                NameHeaderRect.y,
+                NameHeaderRect.width - 8f,
+                NameHeaderRect.height
+            );
+            PriceHeaderRect = NameHeaderRect.ShiftRight(1f);
+            PriceHeaderTextRect = new Rect(
+                PriceHeaderRect.x + 4f,
+                PriceHeaderRect.y,
+                PriceHeaderRect.width - 8f,
+                PriceHeaderRect.height
+            );
+        }
+    }
+}
---
+++ Source/ToolkitUtils/Workers/TraitViewWorker.cs
@@ -0,0 +1,105 @@
+﻿// ToolkitUtils
+// Copyright (C) 2021  SirRandoo
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System.Text;
+using SirRandoo.ToolkitUtils.Helpers;
+using SirRandoo.ToolkitUtils.Models.Tables;
+using UnityEngine;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Workers
+{
+    public class TraitViewWorker : TraitTableWorker
+    {
+        public override void DrawHeaders(Rect canvas)
+        {
+            DrawSortableHeaders();
+            DrawSortableHeaderIcon();
+        }
+
+        protected override void DrawTrait(Rect canvas, TraitTableItem trait)
+        {
+            var nameMouseOverRect = new Rect(NameHeaderRect.x, canvas.y, NameHeaderRect.width, RowLineHeight);
+            var nameRect = new Rect(NameHeaderTextRect.x, canvas.y, NameHeaderTextRect.width, RowLineHeight);
+            var addPriceRect = new Rect(
+                AddPriceHeaderTextRect.x,
+                canvas.y,
+                AddPriceHeaderTextRect.width,
+                RowLineHeight
+            );
+            var removePriceRect = new Rect(
+                RemovePriceHeaderTextRect.x,
+                canvas.y,
+                RemovePriceHeaderTextRect.width,
+                RowLineHeight
+            );
+
+            SettingsHelper.DrawLabel(nameRect, trait.Data.Name);
+
+            if (!trait.EditingName)
+            {
+                Widgets.DrawHighlightIfMouseover(nameMouseOverRect);
+
+                var builder = new StringBuilder();
+                builder.AppendLine(trait.Data.Description);
+                builder.AppendLine();
+
+                foreach (string i in trait.Data.Stats)
+                {
+                    builder.AppendLine($"- {i}");
+                }
+
+                TooltipHandler.TipRegion(nameMouseOverRect, builder.ToString());
+            }
+
+            if (trait.Data.CanAdd)
+            {
+                SettingsHelper.DrawLabel(addPriceRect, trait.Data.CostToAdd.ToString("N0"));
+            }
+
+            if (trait.Data.CanRemove)
+            {
+                SettingsHelper.DrawLabel(removePriceRect, trait.Data.CostToRemove.ToString("N0"));
+            }
+        }
+
+        public override void NotifyResolutionChanged(Rect canvas)
+        {
+            float distributedWidth = Mathf.FloorToInt((canvas.width - 16f) * 0.3333f);
+            NameHeaderRect = new Rect(0f, 0f, distributedWidth, LineHeight);
+            NameHeaderTextRect = new Rect(
+                NameHeaderRect.x + 4f,
+                NameHeaderRect.y,
+                NameHeaderRect.width - 8f,
+                NameHeaderRect.height
+            );
+            AddPriceHeaderRect = NameHeaderRect.ShiftRight(1f);
+            AddPriceHeaderTextRect = new Rect(
+                AddPriceHeaderRect.x + 4f,
+                AddPriceHeaderRect.y,
+                AddPriceHeaderRect.width - 8f,
+                AddPriceHeaderRect.height
+            );
+            RemovePriceHeaderRect = AddPriceHeaderRect.ShiftRight(1f);
+            RemovePriceHeaderTextRect = new Rect(
+                RemovePriceHeaderRect.x + 4f,
+                RemovePriceHeaderRect.y,
+                RemovePriceHeaderRect.width - 8f,
+                RemovePriceHeaderRect.height
+            );
+        }
+    }
+}
