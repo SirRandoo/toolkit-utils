cd5ffd81282b9168ef1b04b2c7bc82a9bf1cb286
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2021-03-16 12:34:33 -0500

    Consolidated table workers into a more reusable generic class.

--- Source/ToolkitUtils/Workers/TableWorker.cs
+++ Source/ToolkitUtils/Workers/TableWorker.cs
@@ -16,50 +16,18 @@
 
 using System;
 using System.Collections.Generic;
-using UnityEngine;
-using Verse;
 
 namespace SirRandoo.ToolkitUtils.Workers
 {
     public enum SortOrder { Ascending, Descending }
 
-    public abstract class TableWorker<T>
+    public abstract class TableWorker<T> : TableWorkerBase
     {
         private protected List<T> _data;
 
-        protected float LineHeight => Mathf.FloorToInt(Text.LineHeight);
-
-        protected float RowLineHeight => Mathf.FloorToInt(Text.LineHeight * 1.333f);
-
         public IEnumerable<T> Data => _data;
 
-        public void Draw(Rect canvas)
-        {
-            var headerRow = new Rect(canvas.x, canvas.y, canvas.width, LineHeight);
-            var tableContents = new Rect(
-                canvas.x,
-                canvas.y + headerRow.height,
-                canvas.width,
-                canvas.height - headerRow.height
-            );
-
-            GUI.BeginGroup(headerRow);
-            DrawHeaders(new Rect(0f, 0f, headerRow.width, headerRow.height));
-            GUI.EndGroup();
-
-            GUI.BeginGroup(tableContents);
-            DrawTableContents(new Rect(0f, 0f, tableContents.width, tableContents.height));
-            GUI.EndGroup();
-        }
-
-        public abstract void DrawHeaders(Rect canvas);
-        public abstract void DrawTableContents(Rect canvas);
-        public abstract void Prepare();
         public abstract void EnsureExists(T data);
-        public abstract void NotifySortRequested();
-        public abstract void NotifySearchRequested(string query);
-        public abstract void NotifyResolutionChanged(Rect canvas);
         public abstract void NotifyCustomSearchRequested(Func<T, bool> worker);
-        protected abstract void FilterDataBySearch(string query);
     }
 }
--- Source/ToolkitUtils/Workers/TableWorkerBase.cs
+++ Source/ToolkitUtils/Workers/TableWorkerBase.cs
@@ -20,10 +20,40 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+using UnityEngine;
+using Verse;
+
 namespace SirRandoo.ToolkitUtils.Workers
 {
-    public class TableWorkerBase
+    public abstract class TableWorkerBase
     {
-        
+        protected float LineHeight => Mathf.FloorToInt(Text.LineHeight);
+        protected float RowLineHeight => Mathf.FloorToInt(Text.LineHeight * 1.333f);
+        public abstract void DrawHeaders(Rect canvas);
+        public abstract void DrawTableContents(Rect canvas);
+        public abstract void Prepare();
+        public abstract void NotifySortRequested();
+        public abstract void NotifySearchRequested(string query);
+        public abstract void NotifyResolutionChanged(Rect canvas);
+        protected abstract void FilterDataBySearch(string query);
+
+        public void Draw(Rect canvas)
+        {
+            var headerRow = new Rect(canvas.x, canvas.y, canvas.width, LineHeight);
+            var tableContents = new Rect(
+                canvas.x,
+                canvas.y + headerRow.height,
+                canvas.width,
+                canvas.height - headerRow.height
+            );
+
+            GUI.BeginGroup(headerRow);
+            DrawHeaders(new Rect(0f, 0f, headerRow.width, headerRow.height));
+            GUI.EndGroup();
+
+            GUI.BeginGroup(tableContents);
+            DrawTableContents(new Rect(0f, 0f, tableContents.width, tableContents.height));
+            GUI.EndGroup();
+        }
     }
 }
