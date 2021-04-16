1c30b5b0139789f084305b2f1d5a5b1fb057268d
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2021-04-16 04:47:21 -0500

    Bumped to beta 18.

--- Source/ToolkitUtils/Properties/AssemblyInfo.cs
+++ Source/ToolkitUtils/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("5.0.0.0")]
 [assembly: AssemblyFileVersion("5.0.3.2")]
-[assembly: AssemblyInformationalVersion("6.0.0.0-beta17")]
+[assembly: AssemblyInformationalVersion("6.0.0.0-beta18")]
