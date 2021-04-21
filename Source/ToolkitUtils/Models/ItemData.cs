561a43473f4847555485b761c7717289aa002979
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2021-04-21 02:24:39 -0500

    Re-added back the research override data to the item data class.

--- Source/ToolkitUtils/Models/ItemData.cs
+++ Source/ToolkitUtils/Models/ItemData.cs
@@ -14,16 +14,22 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Serialization;
 using JetBrains.Annotations;
+using SirRandoo.ToolkitUtils.Helpers;
 using SirRandoo.ToolkitUtils.Interfaces;
 using TwitchToolkit;
+using Verse;
 
 namespace SirRandoo.ToolkitUtils.Models
 {
     [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
     public class ItemData : IShopDataBase
     {
+        private List<ResearchProjectDef> researchProjects = new List<ResearchProjectDef>();
+
         [DataMember(Name = "CustomName")] public string CustomName { get; set; }
 
         [DataMember(Name = "HasQuantityLimit")]
@@ -35,7 +41,55 @@ namespace SirRandoo.ToolkitUtils.Models
         [DataMember(Name = "IsWeapon")] public bool IsWeapon { get; set; }
         [DataMember(Name = "QuantityLimit")] public int QuantityLimit { get; set; } = 1;
         [DataMember(Name = "Weight")] public float Weight { get; set; } = 1f;
+
+        [DataMember(Name = "ResearchOverrides")]
+        public List<string> ResearchOverrides { get; set; }
+
         [DataMember(Name = "Mod")] public string Mod { get; set; }
         [DataMember(Name = "KarmaType")] public KarmaType? KarmaType { get; set; }
+
+        [CanBeNull]
+        public List<ResearchProjectDef> GetResearchOverrides()
+        {
+            if (ResearchOverrides.NullOrEmpty())
+            {
+                return null;
+            }
+
+            if (researchProjects.Count == ResearchOverrides.Count)
+            {
+                return researchProjects;
+            }
+
+            var toCull = new List<string>();
+            foreach (string defName in ResearchOverrides.Where(
+                o => researchProjects.Find(i => i.defName.Equals(o)) == null
+            ))
+            {
+                ResearchProjectDef def = DefDatabase<ResearchProjectDef>.GetNamed(defName, false);
+
+                if (def == null)
+                {
+                    LogHelper.Warn($@"The research project ""{defName}"" could not be found!");
+                    toCull.Add(defName);
+                }
+
+                researchProjects.Add(def);
+            }
+
+            if (toCull.Count <= 0)
+            {
+                return researchProjects;
+            }
+
+            {
+                foreach (string defName in toCull)
+                {
+                    ResearchOverrides.Remove(defName);
+                }
+            }
+
+            return researchProjects;
+        }
     }
 }
