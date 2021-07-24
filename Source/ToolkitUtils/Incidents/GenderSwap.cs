077d9f11d9de51e173e17802052def8f3a664e3d
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2021-07-24 10:23:15 -0500

    Ported the gender swap event to 1.3.

---
+++ Source/ToolkitUtils/Incidents/GenderSwap.cs
@@ -0,0 +1,74 @@
+﻿// ToolkitUtils
+// Copyright (C) 2021  SirRandoo
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using JetBrains.Annotations;
+using RimWorld;
+using SirRandoo.ToolkitUtils.Helpers;
+using SirRandoo.ToolkitUtils.Utils;
+using TwitchToolkit;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Incidents
+{
+    public class GenderSwap : IncidentVariablesBase
+    {
+        private Pawn pawn;
+
+        public override bool CanHappen(string msg, [NotNull] Viewer viewer)
+        {
+            if (!PurchaseHelper.TryGetPawn(viewer.username, out pawn))
+            {
+                MessageHelper.ReplyToUser(viewer.username, "TKUtils.NoPawn".Localize());
+                return false;
+            }
+
+            if (pawn.kindDef?.RaceProps?.hasGenders == true)
+            {
+                return true;
+            }
+
+            MessageHelper.ReplyToUser(viewer.username, "TKUtils.GenderSwap.None".Localize());
+            return false;
+        }
+
+        public override void Execute()
+        {
+            pawn.gender = pawn.gender == Gender.Female ? Gender.Male : Gender.Female;
+            pawn.story.hairColor = PawnHairColors.RandomHairColor(
+                pawn.story.SkinColor,
+                pawn.ageTracker.AgeBiologicalYears
+            );
+            pawn.story.hairDef = PawnStyleItemChooser.RandomHairFor(pawn);
+            pawn.story.bodyType = pawn.story.adulthood == null
+                ? Rand.Value >= 0.5 ? pawn.gender != Gender.Female ? BodyTypeDefOf.Male : BodyTypeDefOf.Female :
+                BodyTypeDefOf.Thin
+                : pawn.story.adulthood.BodyTypeFor(pawn.gender);
+
+            Viewer.Charge(storeIncident);
+
+            MessageHelper.SendConfirmation(
+                Viewer.username,
+                "TKUtils.GenderSwap.Complete".LocalizeKeyed(pawn.gender.ToString())
+            );
+            Find.LetterStack.ReceiveLetter(
+                "TKUtils.GenderSwapLetter.Title".Localize(),
+                "TKUtils.GenderSwapLetter.Description".LocalizeKeyed(Viewer.username, pawn.gender.ToString()),
+                LetterDefOf.NeutralEvent,
+                pawn
+            );
+        }
+    }
+}
