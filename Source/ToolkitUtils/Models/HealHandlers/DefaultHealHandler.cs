f710dbc3d1dff51bff06a6d21b4290d2eb164816
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2021-07-27 02:19:20 -0500

    Commited the default heal handler.

---
+++ Source/ToolkitUtils/Models/HealHandlers/DefaultHealHandler.cs
@@ -0,0 +1,39 @@
+﻿// ToolkitUtils
+// Copyright (C) 2021  SirRandoo
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using JetBrains.Annotations;
+using SirRandoo.ToolkitUtils.Interfaces;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Models
+{
+    public class DefaultHealHandler : IHealHandler
+    {
+        [NotNull] public string Id => "tkutils.handlers.heal";
+
+        public bool IsHealable([NotNull] Hediff hediff)
+        {
+            var extension = hediff.def.GetModExtension<HealExtension>();
+            return extension == null || extension.ShouldHeal;
+        }
+
+        public bool IsHealable([NotNull] BodyPartRecord bodyPart)
+        {
+            var extension = bodyPart.def.GetModExtension<HealExtension>();
+            return extension == null || extension.ShouldHeal;
+        }
+    }
+}
