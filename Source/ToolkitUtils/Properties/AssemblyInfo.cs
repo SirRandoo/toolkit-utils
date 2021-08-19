b9ca7cc631ae83b543d7a3f224d3ee493fd4a813
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2021-08-19 00:38:24 -0500

    Bumped to version v6.1.4.0.

--- Source/ToolkitUtils/Properties/AssemblyInfo.cs
+++ Source/ToolkitUtils/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("6.0.0.0")]
 [assembly: AssemblyFileVersion("6.0.0.0")]
-[assembly: AssemblyInformationalVersion("6.1.3.4")]
+[assembly: AssemblyInformationalVersion("6.1.4.0")]
 
 
 [assembly: InternalsVisibleTo("ToolkitUtils.Ideology")]
