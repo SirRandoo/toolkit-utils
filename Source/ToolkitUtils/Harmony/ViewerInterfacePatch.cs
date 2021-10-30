1b514f024a72e134c70de57a7e82d1fb11a48836
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2021-10-30 03:25:00 -0500

    Added code to fix ToolkitCore's viewer data occasionally being nulled.

---
+++ Source/ToolkitUtils/Harmony/ViewerInterfacePatch.cs
@@ -0,0 +1,55 @@
+﻿// ToolkitUtils
+// Copyright (C) 2021  SirRandoo
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SirRandoo.ToolkitUtils.Helpers;
+using ToolkitCore;
+using ToolkitCore.Database;
+using ToolkitCore.Models;
+using Verse;
+
+namespace SirRandoo.ToolkitUtils.Harmony
+{
+    [HarmonyPatch]
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public static class ViewerInterfacePatch
+    {
+        public static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return AccessTools.PropertyGetter(typeof(Viewers), nameof(Viewers.All));
+        }
+
+        public static void Prefix()
+        {
+            if (ToolkitData.globalDatabase == null)
+            {
+                LogHelper.Warn("ToolkitCore's global database was null. Recreating...");
+                ToolkitData.globalDatabase = new GlobalDatabase();
+            }
+
+            if (!ToolkitData.globalDatabase.viewers.NullOrEmpty())
+            {
+                return;
+            }
+
+            LogHelper.Warn("ToolkitCore's viewer data was null. Recreating...");
+            ToolkitData.globalDatabase.viewers = new List<Viewer>();
+        }
+    }
+}
