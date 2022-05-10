360f78d90866404939b90e665a083e05d21f9298
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2022-05-10 07:23:31 -0500

    Renamed the purchase handler patch to be more consistent.

--- Source/ToolkitUtils.Patches/Events/PurchaseHandlerPatch.ResolveVariables.cs
+++ Source/ToolkitUtils.Patches/Events/PurchaseHandlerPatch.ResolveVariables.cs
@@ -64,6 +64,7 @@ namespace SirRandoo.ToolkitUtils.Harmony
 
             return false;
         }
+        
         private static bool IsOnCooldown(StoreIncidentVariables incident, [NotNull] Viewer viewer)
         {
             if (incident == StoreIncidentDefOf.Item)
