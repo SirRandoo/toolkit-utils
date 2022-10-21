57a8defa30eb35de50d70b9749406063d227420e
sirrandoo <8146689+SirRandoo@users.noreply.github.com>
2022-10-21 02:21:04 -0500

    Overrode ToolkitCore's settings drawer.

---
+++ Source/ToolkitUtils.Patches/Ui/CoreOverridePatch.cs
@@ -0,0 +1,55 @@
+﻿// MIT License
+// 
+// Copyright (c) 2022 SirRandoo
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using JetBrains.Annotations;
+using SirRandoo.CommonLib.Windows;
+using SirRandoo.ToolkitUtils.Windows;
+
+namespace SirRandoo.ToolkitUtils.Harmony
+{
+    [HarmonyPatch]
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    internal static class CoreOverridePatch
+    {
+        private static readonly MethodInfo OpenProxyWindow = AccessTools.Method(typeof(ProxySettingsWindow), "Open", new[] { typeof(ProxySettingsWindow) });
+
+        private static bool Prepare()
+        {
+            return !CompatRegistry.ToolkitCompatible;
+        }
+
+        private static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return AccessTools.Method(typeof(ToolkitCore.ToolkitCore), nameof(ToolkitCore.ToolkitCore.DoSettingsWindowContents));
+        }
+
+        private static bool Prefix()
+        {
+            OpenProxyWindow.Invoke(null, new object[] { new CoreSettingsWindow() });
+
+            return false;
+        }
+    }
+}
