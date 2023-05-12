d754ed7dc6529b4ef55487a0fa86f4a1e2212aac
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2023-05-12 15:57:59 -0500

    Rewrote the thread-safe translation system.
    The mod now asynchronously calls the game's translation system on the game's thread.

---
+++ src/ToolkitUtils.Wrappers.Async/TaskExtensions.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Verse;
+
+namespace ToolkitUtils.Wrappers.Async
+{
+    [StaticConstructorOnStartup]
+    public static class TaskExtensions
+    {
+        private static readonly TaskScheduler MainThreadScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+        private static readonly TaskFactory MainThreadFactory = new TaskFactory(
+            CancellationToken.None,
+            TaskCreationOptions.DenyChildAttach,
+            TaskContinuationOptions.DenyChildAttach | TaskContinuationOptions.RunContinuationsAsynchronously,
+            MainThreadScheduler
+        );
+
+        public static async Task<T> OnMainAsync<T>([NotNull] this Func<T> func) =>
+            await MainThreadFactory.StartNew(func, TaskCreationOptions.RunContinuationsAsynchronously);
+
+        public static async Task<T> OnMainAsync<T, T1>([NotNull] this Func<T1, T> func, T1 arg1)
+        {
+            return await MainThreadFactory.StartNew(() => func(arg1), TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public static async Task<T> OnMainAsync<T, T1, T2>([NotNull] this Func<T1, T2, T> func, T1 arg1, T2 arg2)
+        {
+            return await MainThreadFactory.StartNew(() => func(arg1, arg2), TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public static async Task<T> OnMainAsync<T, T1, T2, T3>([NotNull] this Func<T1, T2, T3, T> func, T1 arg1, T2 arg2, T3 arg3)
+        {
+            return await MainThreadFactory.StartNew(() => func(arg1, arg2, arg3), TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public static async Task<T> OnMainAsync<T, T1, T2, T3, T4>([NotNull] this Func<T1, T2, T3, T4, T> func, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        {
+            return await MainThreadFactory.StartNew(() => func(arg1, arg2, arg3, arg4), TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public static async Task OnMainAsync([NotNull] this Action func)
+        {
+            await MainThreadFactory.StartNew(func, TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public static async Task OnMainAsync<T>([NotNull] this Action<T> func, T arg1)
+        {
+            await MainThreadFactory.StartNew(() => func(arg1), TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public static async Task OnMainAsync<T1, T2>([NotNull] this Action<T1, T2> func, T1 arg1, T2 arg2)
+        {
+            await MainThreadFactory.StartNew(() => func(arg1, arg2), TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public static async Task OnMainAsync<T1, T2, T3>([NotNull] this Action<T1, T2, T3> func, T1 arg1, T2 arg2, T3 arg3)
+        {
+            await MainThreadFactory.StartNew(() => func(arg1, arg2, arg3), TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public static async Task OnMainAsync<T1, T2, T3, T4>([NotNull] this Action<T1, T2, T3, T4> func, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        {
+            await MainThreadFactory.StartNew(() => func(arg1, arg2, arg3, arg4), TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public static async Task OnMainAsync<T1, T2, T3, T4, T5>([NotNull] this Action<T1, T2, T3, T4, T5> func, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+        {
+            await MainThreadFactory.StartNew(() => func(arg1, arg2, arg3, arg4, arg5), TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public static async Task OnMainAsync<T1, T2, T3, T4, T5, T6>(
+            [NotNull] this Action<T1, T2, T3, T4, T5, T6> func,
+            T1 arg1,
+            T2 arg2,
+            T3 arg3,
+            T4 arg4,
+            T5 arg5,
+            T6 arg6
+        )
+        {
+            await MainThreadFactory.StartNew(() => func(arg1, arg2, arg3, arg4, arg5, arg6), TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public static async Task OnMainAsync<T1, T2, T3, T4, T5, T6, T7>(
+            [NotNull] this Action<T1, T2, T3, T4, T5, T6, T7> func,
+            T1 arg1,
+            T2 arg2,
+            T3 arg3,
+            T4 arg4,
+            T5 arg5,
+            T6 arg6,
+            T7 arg7
+        )
+        {
+            await MainThreadFactory.StartNew(() => func(arg1, arg2, arg3, arg4, arg5, arg6, arg7), TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public static async Task OnMainAsync<T1, T2, T3, T4, T5, T6, T7, T8>(
+            [NotNull] this Action<T1, T2, T3, T4, T5, T6, T7, T8> func,
+            T1 arg1,
+            T2 arg2,
+            T3 arg3,
+            T4 arg4,
+            T5 arg5,
+            T6 arg6,
+            T7 arg7,
+            T8 arg8
+        )
+        {
+            await MainThreadFactory.StartNew(() => func(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8), TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        public static async Task OnMainAsync([NotNull] this Task task)
+        {
+            task.Start(MainThreadScheduler);
+
+            await task;
+        }
+
+        public static async Task<T> OnMainAsync<T>([NotNull] this Task<T> task)
+        {
+            task.Start(MainThreadScheduler);
+
+            return await task;
+        }
+    }
+}
---
+++ src/ToolkitUtils.Wrappers.Async/TranslatorExtensions.cs
@@ -0,0 +1,71 @@
+﻿// MIT License
+// 
+// Copyright (c) 2023 SirRandoo
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Verse;
+
+namespace ToolkitUtils.Wrappers.Async
+{
+    public static class TranslatorExtensions
+    {
+        /// <inheritdoc cref="Translator.TranslateSimple"/>
+        public static async Task<string> TranslateSimpleAsync(this string key)
+        {
+            return await TaskExtensions.OnMainAsync(Translator.TranslateSimple, key);
+        }
+
+        /// <inheritdoc cref="TranslatorFormattedStringExtensions.Translate(string, NamedArgument)"/>
+        [ItemNotNull]
+        public static async Task<string> TranslateAsync(this string key, NamedArgument arg1)
+        {
+            return await TaskExtensions.OnMainAsync(TranslatorFormattedStringExtensions.Translate, key, arg1);
+        }
+
+        /// <inheritdoc cref="TranslatorFormattedStringExtensions.Translate(string, NamedArgument, NamedArgument)"/>
+        [ItemNotNull]
+        public static async Task<string> TranslateAsync(this string key, NamedArgument arg1, NamedArgument arg2)
+        {
+            return await TaskExtensions.OnMainAsync(TranslatorFormattedStringExtensions.Translate, key, arg1, arg2);
+        }
+
+        /// <inheritdoc cref="TranslatorFormattedStringExtensions.Translate(string, NamedArgument, NamedArgument, NamedArgument)"/>
+        [ItemNotNull]
+        public static async Task<string> TranslateAsync(this string key, NamedArgument arg1, NamedArgument arg2, NamedArgument arg3)
+        {
+            return await TaskExtensions.OnMainAsync(TranslatorFormattedStringExtensions.Translate, key, arg1, arg2, arg3);
+        }
+
+        /// <inheritdoc cref="Translator.CanTranslate"/>
+        public static async Task<bool> CanTranslate(this string key)
+        {
+            return await TaskExtensions.OnMainAsync(Translator.CanTranslate, key);
+        }
+
+        /// <inheritdoc cref="Translator.TranslateWithBackup"/>
+        [ItemNotNull]
+        public static async Task<string> TranslateWithBackupAsync(this string key, TaggedString backupKey)
+        {
+            return await TaskExtensions.OnMainAsync(Translator.TranslateWithBackup, key, backupKey);
+        }
+    }
+}
