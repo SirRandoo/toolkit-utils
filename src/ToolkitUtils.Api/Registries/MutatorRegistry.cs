51b5cede4a597c818d201ab02053ff34af22d488
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2023-05-16 19:49:30 -0500

    Added a docstring to the MutatorRegistry class.

--- src/ToolkitUtils.Api/Registries/MutatorRegistry.cs
+++ src/ToolkitUtils.Api/Registries/MutatorRegistry.cs
@@ -28,6 +28,15 @@ using Verse;
 
 namespace ToolkitUtils.Api
 {
+    /// <summary>
+    ///     A registry for housing <see cref="IMutator"/> implementations
+    ///     for quick access.
+    /// </summary>
+    /// <remarks>
+    ///     It's important to note that this class does <b>not</b> feature
+    ///     thread safety. If you wish to modify the registry you should do
+    ///     it at startup, never after.
+    /// </remarks>
     [StaticConstructorOnStartup]
     public static class MutatorRegistry
     {
