e8d2ab06fe38b0b1f43d53750a3b8ba18d5edcda
SirRandoo <8146689+SirRandoo@users.noreply.github.com.>
2024-05-17 12:18:53 -0500

    Added a null check to CommandItem.FromToolkit(Command).
    The method previously used the Equals(string) instance method, which threw when the instance was null. It now uses string.Equals(string, string), which doesn't throw when the left side is null.

--- Source/ToolkitUtils/Models/Serialization/CommandItem.cs
+++ Source/ToolkitUtils/Models/Serialization/CommandItem.cs
@@ -49,7 +49,7 @@ public class CommandItem : IUsageItemBase
 
         result.Data = new CommandData
         {
-            IsShortcut = command.commandDriver is { Name: "Buy" } && !command.defName.Equals("Buy"),
+            IsShortcut = command.commandDriver is { Name: "Buy" } && !string.Equals(command.defName, "Buy"),
             Mod = command.TryGetModName(),
             IsBalance = command == CommandDefOf.CheckBalance,
             IsBuy = command == CommandDefOf.Buy
